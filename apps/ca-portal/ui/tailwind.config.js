589fd1c18cde3d45a619d7764186248435f6ced1
AngelASC6 <Angelmichaelvasquez@gmail.com>
2025-06-16 12:54:07 -0400

    added rit colors to Tailwind theme

--- apps/ca-portal/ui/tailwind.config.js
+++ apps/ca-portal/ui/tailwind.config.js
@@ -4,7 +4,16 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "rit-orange": "#F76902",
+        "rit-off-white": "#D7D2CB",
+        "rit-gray": "#A2AAAD",
+        "rit-dark-gray": "#7C878E",
+        "rit-light-gray": "#D0D3D4",
+        "rit-beige": "#ACA39A",
+      },
+    },
   },
   plugins: [],
 }
