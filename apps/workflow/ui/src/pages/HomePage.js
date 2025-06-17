d586fed2fa4787414572a6d45e9a416b36b9e245
AlecKovalczik <aleckovalczik@gmail.com>
2025-06-17 13:27:28 -0400

    Added tailwindcss and react-router-dom, and tried to implement them to make the root directory route to HomePage

---
+++ apps/workflow/ui/src/pages/HomePage.js
@@ -0,0 +1,11 @@
+export default function HomePage() {
+    return (
+        <div>
+            <h1>Welcome to the Workflows App</h1>
+            <p>
+                This application is responsible for providing users at rit with the ability to create, manage, 
+                and complete workflows.
+            </p>
+        </div>
+    )
+}
