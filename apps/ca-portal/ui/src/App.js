ff2c804375250ac1a272388bbb1f0b06bda04b29
av1698@rit.edu <av1698@rit.edu>
2025-06-17 10:19:39 -0400

    added routing

--- apps/ca-portal/ui/src/App.js
+++ apps/ca-portal/ui/src/App.js
@@ -1,12 +1,50 @@
 import './App.css';
 import Landing from './components/Landing';
+import { Routes, Route, Link } from 'react-router';
+
 
 function App() {
   return (
     <div className="App">
-      <Landing/>
+      <Navigation/>
+      <Routes>
+        <Route path="/" element={<Landing/>} />
+        <Route path="/home" element={<Home/>} />
+        <Route path="/users" element={<Users/>} />
+      </Routes>
     </div>
   );
+}
+
+
+const Home = () => {
+  return (
+    <main style={{ padding: '1rem 0' }}>
+      <h2>Home</h2>
+    </main>
+  );
+};
+
+const Users = () => {
+  return (
+    <main style={{ padding: '1rem 0' }}>
+      <h2>Users</h2>
+    </main>
+  );
+};
+
+const Navigation = () => {
+  return(
+    <nav
+      style={{
+        borderBottom: "solid 1px",
+        paddingBottom: "1rem",
+      }}
+    >
+      <Link to="/home">Home</Link>
+      <Link to="/users">Users</Link>
+    </nav>
+  )
 }
 
 export default App;
