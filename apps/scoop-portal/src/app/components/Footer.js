01d096faf3eac4b583aa89f04307b50540fa2018
SierraTran <sierra.tran@mail.com>
2025-06-25 10:38:31 -0400

    Wrote less generic text for the Footer component

--- apps/scoop-portal/src/app/components/Footer.js
+++ apps/scoop-portal/src/app/components/Footer.js
@@ -1,11 +1,20 @@
 import "../css/footer.css";
 
+/**
+ * TODO: Documentation for Footer component
+ *
+ * @returns {JSX.Element}
+ */
 function Footer() {
     return (
         <footer>
-            <h1>This is the footer.</h1>
+            <h1>
+                B. Thomas Golisano College of Computing & Information Sciences
+            </h1>
+            <br />
+            <h2>Department of Software Engineering</h2>
         </footer>
-    )
+    );
 }
 
 export default Footer;
