0194cc38757a1171b8b1003b7f83c2af14090ebf
SierraTran <sierra.tran@mail.com>
2025-06-27 11:03:45 -0400

    Initial commit for /projects page

---
+++ apps/scoop-portal/src/app/projects/page.js
@@ -0,0 +1,8 @@
+export default function Projects() {
+    return (
+        <div>
+            <h1>Welcome to the Projects page.</h1>
+            <p>This is in the layout file for the Projects page.</p>
+        </div>
+    );
+}
