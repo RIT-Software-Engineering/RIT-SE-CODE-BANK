ddc761441e21f043d32f7a9d49b0dd5a426292bb
SierraTran <sierra.tran@mail.com>
2025-06-30 01:01:49 -0400

    Contained children in generic parent element

--- apps/scoop-portal/src/app/projects/[projectId]/layout.js
+++ apps/scoop-portal/src/app/projects/[projectId]/layout.js
@@ -1,8 +1,7 @@
 export default function ProjectDetailsLayout({ children }) {
     return (
-        <div>
-            <p>The page for a specific project will have a different layout than the main Projects page.</p>
+        <>
             {children}
-        </div>
+        </>
     );
 }
