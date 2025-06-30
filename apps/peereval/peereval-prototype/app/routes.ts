86ebd92dcdd7d30d03c7c56a3392811d486b79dc
Raynard Miot <rayhmiot@gmail.com>
2025-06-30 08:54:21 -0400

    Removed prototype

--- apps/peereval/peereval-prototype/app/routes.ts
+++
@@ -1,9 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-    index("routes/home.tsx"),
-    route("peerDashboard", "./routes/peerDashboard.tsx"),
-    route("projects/:pid", "./routes/projectView.tsx"),
-    route("feedbackForm/:fid", "./routes/feedbackForm.tsx"),
-    route("receivedFeedback/:fid", "./routes/receivedFeedback.tsx")
-] satisfies RouteConfig;
