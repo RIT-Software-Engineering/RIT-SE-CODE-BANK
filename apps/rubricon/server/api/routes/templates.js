94294adea972804a19e5d4f37cea4ace928896a7
AlecKovalczik <aleckovalczik@gmail.com>
2025-07-01 13:50:39 -0400

    drafted some data types, redefined some of the database to match requirements, will likely need to add more for ordering.

--- apps/rubricon/server/api/routes/templates.js
+++ apps/rubricon/server/api/routes/templates.js
@@ -4,32 +4,296 @@ const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 /**
+ * Set up the default templates
+ */
+router.get('/init', async (req, res) => {
+    try {
+        await prisma.$transaction(async () => {
+            // await prisma.templates.create({
+            //     data: {
+            //         rubric: {
+            //             create: {
+            //                 title: "Holistic",
+            //                 description: "Holistic rubrics are general...",
+            //                 rows: 6,
+            //                 columns: 2,
+            //                 criteria_column: 1,
+            //                 headers: {
+            //                     create: {
+            //                         titles: {
+            //                             create: [
+            //                                 {
+            //                                     title: "Grade"
+            //                                 },
+            //                                 {
+            //                                     title: "Description"
+            //                                 }
+            //                             ]
+            //                         }
+            //                     }
+            //                 },
+            //                 criteria: {
+            //                     create: [
+            //                         {
+            //                             name: "A",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Excellent"
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "B",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Proficient"
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "C",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Good"
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "D",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Needs Improvement"
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "F",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "You screwed up"
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                     ]
+            //                 }
+            //             }
+            //         }
+            //     }
+            // });
+            // await prisma.templates.create({
+            //     data: {
+            //         rubric: {
+            //             create: {
+            //                 title: "Analytic",
+            //                 description: "Analytic rubrics are complex...",
+            //                 rows: 4,
+            //                 columns: 5,
+            //                 criteria_column: 1,
+            //                 headers: {
+            //                     create: {
+            //                         titles: {
+            //                             create: [
+            //                                 {
+            //                                     title: "Criteria"
+            //                                 },
+            //                                 {
+            //                                     points: 10
+            //                                 },
+            //                                 {
+            //                                     points: 7
+            //                                 },
+            //                                 {
+            //                                     points: 3
+            //                                 },
+            //                                 {
+            //                                     points: 0
+            //                                 }
+            //                             ]
+            //                         }
+            //                     }
+            //                 },
+            //                 criteria: {
+            //                     create: [
+            //                         {
+            //                             name: "Project",
+            //                             description: "",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Project work excelled in A, B, and C."
+            //                                     },
+            //                                     {
+            //                                         description: "level 2."
+            //                                     },
+            //                                     {
+            //                                         description: "level 3."
+            //                                     },
+            //                                     {
+            //                                         description: "level 4."
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "Homework",
+            //                             description: "",
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Project work excelled in A, B, and C."
+            //                                     },
+            //                                     {
+            //                                         description: "level 2."
+            //                                     },
+            //                                     {
+            //                                         description: "level 3."
+            //                                     },
+            //                                     {
+            //                                         description: "level 4."
+            //                                     }
+            //                                 ]
+            //                             }
+            //                         },
+            //                         {
+            //                             name: "Participation",
+            //                             points: 5,
+            //                             levels: {
+            //                                 create: [
+            //                                     {
+            //                                         description: "Student participated at least once during class discussion.",
+            //                                         points: 5
+            //                                     },
+            //                                     {
+            //                                         description: "Student did not participate in class discussion.",
+            //                                         points: 0
+            //                                     },
+            //                                 ]
+            //                             }
+            //                         },
+            //                     ]
+            //                 }
+            //             }
+            //         }
+            //     }
+            // });
+            await prisma.templates.create({
+                data: {
+                    rubric: {
+                        create: {
+                            title: "Analytic",
+                            description: "Analytic rubrics are complex...",
+                            rows: 4,
+                            columns: 3,
+                            criteria_column: 1,
+                            headers: {
+                                create: {
+                                    titles: {
+                                        create: [
+                                            {
+                                                title: "Areas that fell short"
+                                            },
+                                            {
+                                                title: "Criteria"
+                                            },
+                                            {
+                                                title: "Areas of proficiency"
+                                            }
+                                        ]
+                                    }
+                                }
+                            },
+                            criteria: {
+                                create: [
+                                    {
+                                        name: "Project",
+                                        description: "Project work meets requirements A, B, and C.",
+                                        levels: {
+                                            create: [
+                                                {
+                                                },
+                                                {
+                                                }
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        name: "Homework",
+                                        description: "Homework is complete, and submitted on time.",
+                                        points: 10,
+                                        levels: {
+                                            create: [
+                                                {
+                                                },
+                                                {
+                                                }
+                                            ]
+                                        }
+                                    },
+                                    {
+                                        name: "Participation",
+                                        description: "Student participated at least once during class discussion.",
+                                        points: 5,
+                                        levels: {
+                                            create: [
+                                                {
+                                                },
+                                                {
+                                                }
+                                            ]
+                                        }
+                                    },
+                                ]
+                            }
+                        }
+                    }
+                }
+            });
+        });
+        res.status(200).send('Default templates initialized successfully.');
+    } catch (error) {
+        console.error(error)
+        res.status(500).send('There was an error initializing default templates.');
+    }
+})
+
+
+/**
  * Get all templates
  */
 router.get('/', async (req, res) => {
-  try {
-    const templates = await prisma.templates.findMany();
+    try {
+        const templates = await prisma.templates.findMany();
 
-    res.send(templates);
-  } catch (error) {
-    res.status(500).send('There was an error fetching templates.');
-  }
+        res.send(templates);
+    } catch (error) {
+        res.status(500).send('There was an error fetching templates.');
+    }
 });
 
 /**
  * Get a template by id
  */
 router.get('/:id', async (req, res) => {
-  try {
-    const id = req.params.id;
-    const template = await prisma.templates.findUnique({
-    where: { id: id }
-  })
+    try {
+        const id = req.params.id;
+        const template = await prisma.templates.findUnique({
+            where: { id: id }
+        })
 
-    res.send(template);
-  } catch (error) {
-    res.status(500).send('There was an error fetching templates.');
-  }
+        res.send(template);
+    } catch (error) {
+        res.status(500).send('There was an error fetching templates.');
+    }
 });
 
 module.exports = router;
--- apps/rubricon/server/data/prisma/migrations/20250701174852_init/migration.sql
+++ apps/rubricon/server/data/prisma/migrations/20250701174852_init/migration.sql
@@ -22,11 +22,20 @@ CREATE TABLE `Rubrics` (
 -- CreateTable
 CREATE TABLE `Headers` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `rubric_id` VARCHAR(191) NOT NULL,
+
+    UNIQUE INDEX `Headers_rubric_id_key`(`rubric_id`),
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `Titles` (
+    `id` VARCHAR(191) NOT NULL,
+    `title` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
     `points` DOUBLE NULL,
     `weight` DOUBLE NULL,
-    `rubric_id` VARCHAR(191) NOT NULL,
+    `header_id` VARCHAR(191) NOT NULL,
 
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
@@ -34,8 +43,8 @@ CREATE TABLE `Headers` (
 -- CreateTable
 CREATE TABLE `Criteria` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
     `points` DOUBLE NULL,
     `weight` DOUBLE NULL,
     `rubric_id` VARCHAR(191) NOT NULL,
@@ -46,8 +55,8 @@ CREATE TABLE `Criteria` (
 -- CreateTable
 CREATE TABLE `Levels` (
     `id` VARCHAR(191) NOT NULL,
-    `name` VARCHAR(191) NOT NULL,
-    `description` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(191) NULL,
+    `description` VARCHAR(191) NULL,
     `points` DOUBLE NULL,
     `weight` DOUBLE NULL,
     `criterion_id` VARCHAR(191) NOT NULL,
@@ -56,13 +65,16 @@ CREATE TABLE `Levels` (
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- AddForeignKey
-ALTER TABLE `Templates` ADD CONSTRAINT `Templates_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Templates` ADD CONSTRAINT `Templates_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Headers` ADD CONSTRAINT `Headers_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Headers` ADD CONSTRAINT `Headers_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Criteria` ADD CONSTRAINT `Criteria_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Titles` ADD CONSTRAINT `Titles_header_id_fkey` FOREIGN KEY (`header_id`) REFERENCES `Headers`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `Levels` ADD CONSTRAINT `Levels_criterion_id_fkey` FOREIGN KEY (`criterion_id`) REFERENCES `Criteria`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `Criteria` ADD CONSTRAINT `Criteria_rubric_id_fkey` FOREIGN KEY (`rubric_id`) REFERENCES `Rubrics`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `Levels` ADD CONSTRAINT `Levels_criterion_id_fkey` FOREIGN KEY (`criterion_id`) REFERENCES `Criteria`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
