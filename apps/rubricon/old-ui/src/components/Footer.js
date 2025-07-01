abbf7b8084c31ca9c2ab343da206fa32d323146b
AlecKovalczik <aleckovalczik@gmail.com>
2025-07-01 10:57:10 -0400

    deleted old-ui because it never should have been added here in the first place

--- apps/rubricon/old-ui/src/components/Footer.js
+++
@@ -1,9 +0,0 @@
-import "../styles/components/Footer.css";
-
-export default function Footer() {
-    return (
-        <footer className="App-footer">
-            <p>© 2025 Rubricon Prototype. All rights reserved.</p>
-        </footer>
-    )
-}
