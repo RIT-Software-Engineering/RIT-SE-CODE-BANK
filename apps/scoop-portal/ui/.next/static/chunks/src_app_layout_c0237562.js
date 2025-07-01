af5cd67ec33940a0bf0fdcb81a72c0f0c12247ef
dcorcoran8543 <dcorcoran513@gmail.com>
2025-07-01 19:41:10 -0400

    Removed .next folder from repo

--- apps/scoop-portal/ui/.next/static/chunks/src_app_layout_c0237562.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/[root-of-the-server]__a2f1cdd3._.css",
-  "static/chunks/node_modules_tailwindcss_dist_779531cc._.js"
-],
-    source: "dynamic"
-});
