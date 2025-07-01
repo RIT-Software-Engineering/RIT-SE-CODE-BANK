8439e08c34d1788304b946de79ac7961b0f26a03
AlecKovalczik <aleckovalczik@gmail.com>
2025-07-01 09:12:34 -0400

    updated

---
+++ apps/rubricon/ui/.next/static/chunks/src_app_layout_c0237562.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/[root-of-the-server]__1c72d2dd._.css",
+  "static/chunks/_9c6ad7a8._.js"
+],
+    source: "dynamic"
+});
