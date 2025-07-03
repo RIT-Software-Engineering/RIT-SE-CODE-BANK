3154ed62fde09d0e73b2f0731822b297d7d3c107
AlecKovalczik <aleckovalczik@gmail.com>
2025-07-03 12:06:44 -0400

    started creating the action state thing so they can step through the app's state, tried using it on the previe page, getting chunk error.

--- apps/workflow/ui/src/app/dashboard/preview/[id]/page.js
+++ apps/workflow/ui/src/app/dashboard/preview/[id]/page.js
@@ -1,9 +1,9 @@
-import ActionList from "@/components/actions/ActionList";
 import Link from "next/link";
+import ActionState from "@/components/actions/ActionState"
 
 export default async function PreviewPage({ params }) {
     const { id } = await params;
-    const url = process.env.SERVER_URL || ""
+    const url = process.env.SERVER_URL || "http://localhost:3001";
     const data = await fetch(url + "/workflows?workflowId=" + id,
         {
             method: "GET",
@@ -13,6 +13,7 @@ export default async function PreviewPage({ params }) {
         }
     )
     const workflow = (await data.json())[0];
+    console.log(workflow);
 
     return (
         <>
@@ -22,33 +23,10 @@ export default async function PreviewPage({ params }) {
                 </div>
                 <h1 className="text-5xl font-bold w-fit mx-auto text-center">Preview</h1>
                 <div className="flex flex-row gap-2 items-center justify-end h-full pr-4">
-                    {/* <Link href={"/"} className="border p-2 rounded-sm min-w-24 text-center">Other</Link> */}
                 </div>
             </div>
-            <div className="flex flex-col gap-4 my-4 w-4/5 m-auto border rounded-xl p-4">
-                <div>
-                    <h2 className="text-2xl font-bold">Metadata</h2>
-                    <div className="flex flex-col gap-2 border border-solid border-black rounded-xl p-2">
-                        {(workflow.base_action.metadata).map((data) => (
-                            <div key={data.id} className="border border-solid border-black rounded-md p-2">
-                                {data.key + ": " + data.value}
-                            </div>
-                        ))}
-                    </div>
-                </div>
-                <div>
-                    <h2 className="text-2xl font-bold">Attributes</h2>
-                    <div className="flex flex-col gap-2 border border-solid border-black rounded-xl p-2">
-                        <div>{"Type: " + workflow.base_action.action_type}</div>
-                        <div>{"Frozen: " + workflow.base_action.is_frozen}</div>
-                    </div>
-                </div>
-            </div>
-            <div className="w-4/5 m-auto p-4">
-                <h2 className="text-2xl font-bold">Actions</h2>
-                <div className="border border-solid border-black rounded-xl p-4 mx-[-16]">
-                    <ActionList searchParams={{ "workflowId": workflow.id }} />
-                </div>
+            <div>
+                <ActionState actionId={workflow.root_action_id} />
             </div>
         </>
     )
