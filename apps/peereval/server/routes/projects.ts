4c4fe0c5d5358da84f9810720d905482614437ab
Raynard Miot <rayhmiot@gmail.com>
2025-07-08 13:58:15 -0400

    formatted

--- apps/peereval/server/routes/projects.ts
+++ apps/peereval/server/routes/projects.ts
@@ -7,49 +7,49 @@ const prisma = new PrismaClient();
 // Get all projects
 // /projects
 router.get("/", async (req, res) => {
-  const projects = await prisma.project.findMany({
-    orderBy: {
-      name: "asc",
-    },
-  });
+    const projects = await prisma.project.findMany({
+        orderBy: {
+            name: "asc",
+        },
+    });
 
-  res.json(projects);
+    res.json(projects);
 });
 
 // Get project as peer
 // /projects/asPeer/:userId
 router.get("/asPeer/:userId", async (req, res) => {
-  const userId = req.params.userId;
+    const userId = req.params.userId;
 
-  const projects = await prisma.user.findMany({
-    select: {
-      projectsAsPeers: {},
-    },
-    where: {
-      id: userId,
-    },
-  });
+    const projects = await prisma.user.findMany({
+        select: {
+            projectsAsPeers: {},
+        },
+        where: {
+            id: userId,
+        },
+    });
 
-  if (projects.length == 0) {
-    res.sendStatus(404);
-  } else res.json(projects[0].projectsAsPeers);
+    if (projects.length == 0) {
+        res.sendStatus(404);
+    } else res.json(projects[0].projectsAsPeers);
 });
 
 // Get project as overseer
 // /projects/asOverseer/:userId
 router.get("/asOverseer/:userId", async (req, res) => {
-  const userId = req.params.userId;
+    const userId = req.params.userId;
 
-  const projects = await prisma.user.findMany({
-    select: {
-      projectsAsOverseer: {},
-    },
-    where: {
-      id: userId,
-    },
-  });
+    const projects = await prisma.user.findMany({
+        select: {
+            projectsAsOverseer: {},
+        },
+        where: {
+            id: userId,
+        },
+    });
 
-  res.json(projects[0].projectsAsOverseer);
+    res.json(projects[0].projectsAsOverseer);
 });
 
 export default router;
@@ -57,17 +57,17 @@ export default router;
 // Get peers for a project
 // /projects/getPeers/:id
 router.get("/getPeers/:id", async (req, res) => {
-  const id = req.params.id;
+    const id = req.params.id;
 
-  const peers = await prisma.user.findMany({
-    where: {
-      projectsAsPeers: {
-        some: {
-          id: id,
+    const peers = await prisma.user.findMany({
+        where: {
+            projectsAsPeers: {
+                some: {
+                    id: id,
+                },
+            },
         },
-      },
-    },
-  });
+    });
 
-  res.json(peers);
+    res.json(peers);
 });
