ebb8955c11c1906c5bce57685ac494610ef8e586
Raynard Miot <rayhmiot@gmail.com>
2025-07-09 14:10:43 -0400

    Made footer debug button able to select any user

--- apps/peereval/peereval-app/components/Footer.tsx
+++ apps/peereval/peereval-app/components/Footer.tsx
@@ -1,45 +1,44 @@
 "use client";
 
 import { useAuth } from "@/context/UserContext";
-import { getUserProfile, getUserProfileByEmail } from "@/services/user";
+import { getAllUserProfiles } from "@/services/user";
 import { UserProfile } from "@/types/userProfile";
 import { useEffect, useState } from "react";
 
 export default function Footer() {
     const { currentUser, setCurrentUser } = useAuth();
-    const [aliceUser, setAliceUser] = useState<UserProfile>({
-        id: "d117ad29-814d-47de-aa4b-11723a7440de",
-        name: "Alice",
-        email: "alice@rit.edu",
-    });
-    const [zebraUser, setZebraUser] = useState<UserProfile>({
-        id: "2a18ce36-0b42-475b-855a-0aa62fa4ba07",
-        name: "Zebra",
-        email: "zebra@rit.edu",
-    });
+    const [users, setUsers] = useState<UserProfile[]>([]);
 
     useEffect(() => {
-        const getTheInfos = async () => {
-            setAliceUser(await getUserProfileByEmail("alice@rit.edu"));
-            setZebraUser(await getUserProfileByEmail("zebra@rit.edu"));
-        };
-
-        getTheInfos();
+        (async () => {
+            const us = await getAllUserProfiles();
+            setUsers(us);
+        })();
     }, []);
 
     return (
         <footer className="mt-4 p-4 bg-black text-white text-center">
             <p>© 2025 Peer Eval. All rights reserved.</p>
-            <button
-                className="mt-2 px-4 py-2 bg-white text-black rounded"
-                onClick={() =>
-                    setCurrentUser(
-                        currentUser?.id === aliceUser.id ? zebraUser : aliceUser
-                    )
-                }
-            >
-                Toggle User ID (Current: {currentUser?.name ?? "None"})
-            </button>
+            <div className="flex items-center justify-center mt-2">
+                <span className="mr-2">Current user:</span>
+                <select
+                    className="px-4 py-2 bg-white text-black rounded"
+                    value={currentUser?.id ?? ""}
+                    onChange={(e) => {
+                        const selectedId = e.target.value;
+                        setCurrentUser(users.find((u) => u.id == selectedId)!);
+                    }}
+                >
+                    <option value="" disabled>
+                        Select User
+                    </option>
+                    {users.map((u) => (
+                        <option key={u.id} value={u.id}>
+                            {u.name}
+                        </option>
+                    ))}
+                </select>
+            </div>
         </footer>
     );
 }
