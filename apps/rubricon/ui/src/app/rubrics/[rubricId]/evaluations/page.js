705dbc3a38f0162a5abb6ea6e44d107e4584c7cd
AlecKovalczik <aleckovalczik@gmail.com>
2025-07-14 09:14:15 -0400

    started creating an evaluation page. No content yet.

--- apps/rubricon/ui/src/app/rubrics/[rubricId]/evaluations/page.js
+++ apps/rubricon/ui/src/app/rubrics/[rubricId]/evaluations/page.js
@@ -2,13 +2,13 @@ import EvaluationList from "@/components/EvaluationList"
 import Link from "next/link"
 
 export default async function RubricEvaluationsPage({params}) {
-    const { id } = await params;
+    const { rubricId } = await params;
 
     return (
         <>
             <div className="flex flex-col items-center justify-center gap-4">
                 <div className="grid grid-cols-3 items-center p-4 w-full text-white font-bold bg-primary">
-                    <div></div>
+                    <Link href={`/rubrics/${rubricId}`} className="text-xl hover:text-black mr-auto">{"Cancel"}</Link>
                     <h1 className="text-4xl mx-auto">My Evaluations</h1>
                     <Link href="/create" className="text-xl hover:text-black ml-auto">{"Create"}</Link>
                 </div>
