46106e8c73e386245dc3c11d0f5451b4c26a1778
SierraTran <sierra.tran@mail.com>
2025-07-14 09:47:10 -0400

    Added one entry with blank notes to the test data

--- apps/scoop-portal/server/server/prisma/test-data/sample_journal_entries.js
+++ apps/scoop-portal/server/server/prisma/test-data/sample_journal_entries.js
@@ -1,7 +1,12 @@
 export const sampleJournalEntries = [
     {
-        date: new Date("2025-06-13"),
+        date: new Date("2025-06-13T12:00:00Z"), // in UTC
         contactee: "Alice Johnson",
         notes: "Edit this note for testing.",
+    },
+    {
+        date: new Date("2025-06-20T10:30:00Z"), // in UTC
+        contactee: "John Smith",
+        notes: "",
     },
 ];
