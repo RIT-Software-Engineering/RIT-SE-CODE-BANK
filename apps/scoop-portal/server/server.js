7f3f97ebaec2f8ded923aeb2fef05faff240d7af
SierraTran <sierra.tran@mail.com>
2025-07-14 00:49:33 -0400

    Added journal entry api routes to the server

--- apps/scoop-portal/server/server.js
+++ apps/scoop-portal/server/server.js
@@ -1,14 +1,16 @@
 import express from "express";
 import cors from "cors";
 import applicationRoutes from "./api/application/route.js";
+import journalRoutes from "./api/journal/route.js";
 
 const app = express();
 const PORT = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 app.use("/api/application", applicationRoutes);
+app.use("/api/journal", journalRoutes);
 
 app.listen(PORT, () => {
     //load data?
-  console.log(`Express server is running on port ${PORT}`);
+    console.log(`Express server is running on port ${PORT}`);
 });
