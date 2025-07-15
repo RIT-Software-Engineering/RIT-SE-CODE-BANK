6cad36beea28b109be8b1038a7341b9f845ad217
SierraTran <sierra.tran@mail.com>
2025-07-15 14:27:47 -0400

    Created custom theme with material ui

---
+++ apps/scoop-portal/ui/src/app/_styles/theme.js
@@ -0,0 +1,91 @@
+import { createTheme, ThemeProvider, styled } from "@mui/material";
+
+const theme = createTheme({
+    palette: {
+        primary: {
+            main: "#F76902",
+        },
+        secondary: {
+            main: "#000000",
+        },
+        error: {
+            main: "#DA291C",
+        },
+        warning: {
+            main: "#F6BE00",
+        },
+        info: {
+            main: "#009CBD",
+        },
+        success: {
+            main: "#84BD00",
+        },
+    },
+    typography: {
+        h1: {
+            fontSize: "2rem",
+            lineHeight: "2.5rem",
+            fontWeight: 900,
+        },
+        h2: {
+            fontSize: "1.5rem",
+            lineHeight: "2rem",
+            fontWeight: 500,
+        },
+        h3: {
+            fontSize: "1.25rem",
+            lineHeight: "1.75rem",
+            fontWeight: 300,
+        },
+        body1: {
+            fontSize: "1rem",
+            lineHeight: "1.5rem",
+            fontWeight: 400,
+            marginBottom: "1rem",
+        },
+    },
+    components: {
+        MuiButton: {
+            styleOverrides: {
+                root: {
+                    borderRadius: "0px",
+                    variants: [
+                        {
+                            props: { variant: "solid-orange" },
+                            style: {
+                                backgroundColor: "#F76902",
+                                color: "#FFFFFF",
+                                "&:hover": {
+                                    backgroundColor: "#000000",
+                                },
+                            },
+                        },
+                        {
+                            props: { variant: "solid-gray" },
+                            style: {
+                                backgroundColor: "#D0D3D4",
+                                color: "#000000",
+                                "&:hover": {
+                                    backgroundColor: "#A2AAAD",
+                                },
+                            },
+                        },
+                        {
+                            props: { variant: "outline-orange" },
+                            style: {
+                                border: "1px solid #F76902",
+                                color: "#F76902",
+                                "&:hover": {
+                                    backgroundColor: "#F76902",
+                                    color: "#FFFFFF",
+                                },
+                            },
+                        },
+                    ],
+                },
+            },
+        },
+    },
+});
+
+export default theme;
