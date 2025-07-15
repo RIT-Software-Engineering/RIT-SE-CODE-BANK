aaa11ee80d64087f63aa9ca02966ade2f1798823
ast5382 <ast5382@rit.edu>
2025-07-15 13:05:47 -0400

    added dialog view

--- apps/scoop-portal/ui/src/app/scoopdinator/scooployees/view/page.js
+++ apps/scoop-portal/ui/src/app/scoopdinator/scooployees/view/page.js
@@ -1,12 +1,15 @@
 "use client";
 import React, { useState, useEffect } from 'react';
 import Header from '@components/Header';
-import { Container, Typography, Paper, Table, TableHead, TableCell, TableRow, TableBody, Button } from '@mui/material';
+import { Container, Typography, Paper, Table, TableHead, 
+    TableCell, TableRow, TableBody, Button, Dialog, DialogTitle, DialogContent, Box,
+    DialogActions } from '@mui/material';
 
 
 
 export default function ViewScooployees() {
   const [employees, setEmployees] = useState([]);
+  const [selectedEmployee, setSelectedEmployee] = useState(null);
   const [filter, setFilter] = useState("all"); 
 
   useEffect(() => {
@@ -25,12 +28,30 @@ export default function ViewScooployees() {
         fetchEmployees();
     }, []);
 
+    const handleOpen = (emp) => {
+        setSelectedEmployee({ ...emp, hasBeenRead: true });
+        setEmployees((prev) =>
+            prev.map((e) => (e.id === emp.id ? { ...e, hasBeenRead: true } : e))
+        );
+
+        console.log("opening employee:", selectedEmployee.firstName); //needs useEffect outside of function
+    };
+
+    const handleClose = () => setSelectedEmployee(null);
+
+
   const filteredEmployees =
         filter === "all"
             ? employees
             : employees.filter((employee) => employee.status === filter);
+
+
+            
   return (
     <>
+    <Typography variant="h4" sx={{ fontWeight: 600, mb: 3 }}>
+                    View Scooployees
+                </Typography>
       <Paper elevation={1}>
                           <Table>
                               <TableHead sx={{ backgroundColor: "#F76902" }}>
@@ -72,7 +93,7 @@ export default function ViewScooployees() {
                                           <TableCell align="right">
                                               <Button
                                                   variant="outlined"
-                                                  onClick={() => handleOpen(app)}
+                                                  onClick={() => handleOpen(employee)}
                                                   sx={{
                                                       borderColor: "#F76902",
                                                       color: "#F76902",
@@ -90,6 +111,78 @@ export default function ViewScooployees() {
                               </TableBody>
                           </Table>
                       </Paper>
+
+                      <Dialog
+                                          open={!!selectedEmployee}
+                                          onClose={handleClose}
+                                          maxWidth="sm"
+                                          fullWidth
+                                      >
+                                          {selectedEmployee && (
+                                              <>
+                                                  <DialogTitle
+                                                      sx={{
+                                                          bgcolor: "#F76902",
+                                                          color: "#fff",
+                                                          fontWeight: 600,
+                                                      }}
+                                                  >
+                                                      Employee:{" "}
+                                                      {selectedEmployee.fname} {selectedEmployee.lname}
+                                                  </DialogTitle>
+                                                  <DialogContent dividers>
+                                                      <Typography>
+                                                          <strong>Email:</strong>{" "}
+                                                          {selectedEmployee.email}
+                                                      </Typography>
+                                                      <Typography>
+                                                          <strong>Semester Hired:</strong>{" "}
+                                                          {selectedEmployee.semesterGroup}
+                                                      </Typography>
+                                                      <Typography mt={2} sx={{ fontStyle: "italic" }}>
+                                                          {JSON.stringify(selectedEmployee)}
+                                                      </Typography>
+                      
+                                                  </DialogContent>
+
+                                                  <DialogActions sx={{ px: 3, py: 2 }}>
+                                                      <Button
+                                                          variant="contained"
+                                                        //   onClick={() =>
+                                                        //     //   message function
+                                                        
+                                                        //   }
+                                                          sx={{
+                                                              bgcolor: "#84BD00",
+                                                              "&:hover": { bgcolor: "#6da400" },
+                                                          }}
+                                                      >
+                                                          Message
+                                                      </Button>
+                                                      <Button
+                                                          variant="contained"
+                                                        //   onClick={() =>
+                                                        //       //open journal function
+                                                              
+                                                        //   }
+                                                          sx={{
+                                                              bgcolor: "#84BD00",
+                                                              "&:hover": { bgcolor: "#6da400" },
+                                                          }}
+                                                      >
+                                                          Journal
+                                                      </Button>
+                                                      <Button
+                                                          onClick={handleClose}
+                                                          variant="outlined"
+                                                          color="inherit"
+                                                      >
+                                                          Close
+                                                      </Button>
+                                                  </DialogActions>
+                                              </>
+                                          )}
+                                      </Dialog>
     </>
   );
 }
