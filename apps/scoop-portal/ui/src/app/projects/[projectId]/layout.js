2482244a3d64084ecf8a59a6f6b11038892eacc3
SierraTran <sierra.tran@mail.com>
2025-07-16 13:06:43 -0400

    Moved back button to the page.js file

--- apps/scoop-portal/ui/src/app/projects/[projectId]/layout.js
+++ apps/scoop-portal/ui/src/app/projects/[projectId]/layout.js
@@ -1,13 +1,5 @@
 import { Button } from "@mui/material";
-import { ArrowBack } from "@mui/icons-material";
 
 export default function ProjectDetailsLayout({ children }) {
-    return (
-        <div>
-            <Button href="/projects" startIcon={<ArrowBack />}>
-                Back to Projects
-            </Button>
-            {children}
-        </div>
-    );
+    return <div>{children}</div>;
 }
