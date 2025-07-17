c0e1b7a99577c41841b4d1fb49b485fa9070603f
ast5382 <ast5382@rit.edu>
2025-07-17 13:37:37 -0400

    include dotenv as a dependency and import dotenv in server.js

--- apps/scoop-portal/server/server.js
+++ apps/scoop-portal/server/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import cors from "cors";
 import applicationRoutes from "./api/application/route.js";
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 app.use("/api/application", applicationRoutes);
