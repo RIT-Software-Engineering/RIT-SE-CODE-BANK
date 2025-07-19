64aac62b29f3d2f4c2b53e6ac4e64102912211f3
SierraTran <sierra.tran@mail.com>
2025-07-19 15:54:40 -0400

    Migrated changes for project table

---
+++ apps/scoop-portal/server/server/prisma/models/migrations/20250718193245_optional_project_fields/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE `project` MODIFY `poster` VARCHAR(255) NULL,
+    MODIFY `synopsis` VARCHAR(255) NULL,
+    MODIFY `video` VARCHAR(255) NULL,
+    MODIFY `website` VARCHAR(255) NULL;
