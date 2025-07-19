57aa3c955b6e0232fa3fd752eaad1f5728533a6f
SierraTran <sierra.tran@mail.com>
2025-07-19 16:07:13 -0400

    Added three semester groups for test dataw

---
+++ apps/scoop-portal/server/server/prisma/test-data/sample_semester_groups.js
@@ -0,0 +1,23 @@
+export const sampleSemesterGroups = [
+    {
+        id: 1,
+        name: "2018-19 Fall / Spring",
+        dept: "SE",
+        start_date: new Date("2018-08-27"),
+        end_date: new Date("2019-05-08"),
+    },
+    {
+        id: 2,
+        name: "2019-19 Spring / Summer",
+        dept: "SE",
+        start_date: new Date("2019-01-13"),
+        end_date: new Date("2019-08-07"),
+    },
+    {
+        id: 3,
+        name: "2019-20 Fall / Spring",
+        dept: "SE",
+        start_date: new Date("2024-05-27"),
+        end_date: new Date("2024-08-08"),
+    },
+];
