a97f52b9fe67be064bc916aefd8bba8a9dde67f4
SierraTran <sierra.tran@mail.com>
2025-07-19 15:57:55 -0400

    Added messages for success or errors and exported router

--- apps/scoop-portal/server/api/semestergroup/route.js
+++ apps/scoop-portal/server/api/semestergroup/route.js
@@ -14,7 +14,7 @@ router.get("/", async (req, res) => {
         const semesters = await prisma.semester_Group.findMany();
         res.status(200).json(semesters);
     } catch (error) {
-        console.error();
+        console.error("Error fetching semester groups: ", error);
         res.status(500).json({ message: "Error fetching semester groups." });
     }
 });
@@ -30,11 +30,16 @@ router.post("/", async (req, res) => {
         const newSemester = await prisma.semester_Group.create({
             data: { name, dept, start_date, end_date },
         });
-        res.status(200).json(newSemester);
+        res.status(200).json({
+            message: "New semester group created.",
+            semesterGroup: newSemester,
+        });
     } catch (error) {
         console.error("Error creating semsester group: ", error);
         res.status(500).json({ message: "Error creating semester group" });
     }
 });
 
-// TODO: post method for semester group
+// TODO: put method for semester group
+
+export default router;
