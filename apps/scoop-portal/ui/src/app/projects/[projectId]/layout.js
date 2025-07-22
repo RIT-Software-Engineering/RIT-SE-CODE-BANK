2ca8c2cfac63a0ff0def89b287374ec12b053500
SierraTran <sierra.tran@mail.com>
2025-07-22 14:06:27 -0400

    Removed Header from layout due to errors

--- apps/scoop-portal/ui/src/app/projects/[projectId]/layout.js
+++ apps/scoop-portal/ui/src/app/projects/[projectId]/layout.js
@@ -1,10 +1,5 @@
 import Header from "@components/Header";
 
 export default function ProjectDetailsLayout({ children }) {
-    return (
-        <div>
-            <Header />
-            {children}
-        </div>
-    );
+    return <div>{children}</div>;
 }
