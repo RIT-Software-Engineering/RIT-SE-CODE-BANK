a550a449779639d2caf2747ad3cace7a5994afb2
SierraTran <sierra.tran@mail.com>
2025-07-24 02:51:04 -0400

    Rearranged components into the correct order

--- apps/scoop-portal/ui/src/app/ThemeRegistry.js
+++ apps/scoop-portal/ui/src/app/ThemeRegistry.js
@@ -7,21 +7,23 @@ import { lightTheme, darkTheme } from "@styles/theme";
 export const ThemeContext = createContext({ toggleTheme: () => {} });
 
 export default function ThemeRegistry({ children }) {
-    const [mode, setMode] = useState("light");
+  const [mode, setMode] = useState("light");
 
-    const toggleTheme = () => {
-        setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
-    };
+  const toggleTheme = () => {
+    setMode((prevMode) => (prevMode === "light" ? "dark" : "light"));
+  };
 
-    const theme = useMemo(
-        () => (mode === "light" ? lightTheme : darkTheme),
-        [mode]
-    );
+  const theme = useMemo(
+    () => (mode === "light" ? lightTheme : darkTheme),
+    [mode]
+  );
 
-    return (
-        <ThemeContext.Provider value={{ toggleTheme }}>
-            <CssBaseline />
-            <ThemeProvider theme={theme}>{children}</ThemeProvider>
-        </ThemeContext.Provider>
-    );
+  return (
+    <ThemeContext.Provider value={{ toggleTheme }}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  );
 }
