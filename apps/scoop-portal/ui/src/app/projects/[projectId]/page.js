5b3fe1ebbc27a66178a4bf234968d91ae1680366
SierraTran <sierra.tran@mail.com>
2025-07-24 07:38:39 -0400

    Padding Y adjustment for main container; using theme instead of just baseTheme

--- apps/scoop-portal/ui/src/app/projects/[projectId]/page.js
+++ apps/scoop-portal/ui/src/app/projects/[projectId]/page.js
@@ -1,192 +1,179 @@
 "use client";
 
 import {
-    Box,
-    Button,
-    Card,
-    CardContent,
-    Container,
-    Typography,
-    Paper,
+  Box,
+  Button,
+  Card,
+  CardContent,
+  Container,
+  Typography,
+  Paper,
+  useTheme,
 } from "@mui/material";
 import ArrowBackOutlinedIcon from "@mui/icons-material/ArrowBackOutlined";
 import { EditOutlined } from "@mui/icons-material";
 import React, { useEffect, useState } from "react";
 import ProjectDetailsLoading from "./loading";
 import Header from "@components/Header";
-import baseTheme from "@styles/theme";
 
 export default function ProjectDetails({ params }) {
-    const { projectId } = React.use(params);
-    const [isLoading, setIsLoading] = useState(true);
-    const [project, setProject] = useState([]);
+  const theme = useTheme();
+  const { projectId } = React.use(params);
+  const [isLoading, setIsLoading] = useState(true);
+  const [project, setProject] = useState([]);
 
-    useEffect(() => {
-        console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
-        const fetchProject = async () => {
-            try {
-                const res = await fetch(
-                    `${process.env.NEXT_PUBLIC_API_URL}/api/project/${projectId}`
-                );
-                const data = await res.json();
-                setProject(data);
-            } catch (err) {
-                console.error("Failed to fetch project: ", err);
-            } finally {
-                setIsLoading(false);
-            }
-        };
+  useEffect(() => {
+    console.log("API URL:", process.env.NEXT_PUBLIC_API_URL);
+    const fetchProject = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/project/${projectId}`
+        );
+        const data = await res.json();
+        setProject(data);
+      } catch (err) {
+        console.error("Failed to fetch project: ", err);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-        if (projectId) fetchProject();
-    }, [projectId]);
+    if (projectId) fetchProject();
+  }, [projectId]);
 
-    if (isLoading) {
-        return <ProjectDetailsLoading />;
-    }
+  if (isLoading) {
+    return <ProjectDetailsLoading />;
+  }
 
-    return (
-        <>
-            <Header />
+  return (
+    <>
+      <Header />
 
-            <Container>
-                <Button
-                    href="/projects"
-                    startIcon={<ArrowBackOutlinedIcon />}
-                    variant="outline-orange"
-                >
-                    Back to Projects
-                </Button>
-                <Paper
-                    sx={{
-                        marginTop: "1rem",
-                        padding: "1rem",
-                    }}
-                >
-                    <Container
-                        disableGutters
-                        sx={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            px: "0",
-                        }}
-                    >
-                        <Typography variant="h1">
-                            {project.display_name || project.title}
-                        </Typography>
-                        <Button
-                            startIcon={<EditOutlined />}
-                            variant="solid-orange"
-                            onClick={() => {
-                                alert("Edit functionality not implemented yet");
-                            }}
-                        >
-                            Edit Details
-                        </Button>
-                    </Container>
-                    <Box
-                        sx={{
-                            marginBlock: "1rem",
-                            padding: "0.5em 1em",
-                            display: "inline-block",
-                            backgroundColor:
-                                project.status === "active"
-                                    ? "rgba(0, 156, 189, 0.2)"
-                                    : project.status === "in progress"
-                                      ? "rgba(246, 190, 0, 0.2)"
-                                      : project.status === "completed"
-                                        ? "rgba(132, 189, 0, 0.2)"
-                                        : "rgba(124, 135, 142, 0.2)",
-                            color:
-                                project.status === "active"
-                                    ? baseTheme.palette.info.main
-                                    : project.status === "in progress"
-                                      ? baseTheme.palette.warning.main
-                                      : project.status === "completed"
-                                        ? baseTheme.palette.success.main
-                                        : "rgb(124, 135, 142)",
-                        }}
-                    >
-                        <Typography sx={{ margin: "0" }}>
-                            {project.status.toUpperCase()}
-                        </Typography>
-                    </Box>
-                    <Typography>{project.description}</Typography>
-                    <Container
-                        disableGutters
-                        sx={{
-                            display: "flex",
-                            justifyContent: "space-between",
-                            px: "0",
-                        }}
-                    >
-                        <Card
-                            variant="outlined"
-                            sx={{
-                                width: "45%",
-                                padding: "0.25rem 1rem",
-                            }}
-                        >
-                            <CardContent>
-                                <Typography variant="h3">
-                                    Challenges:
-                                </Typography>
-                                <Typography>
-                                    {project.project_challenges}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                        <Card
-                            variant="outlined"
-                            sx={{
-                                width: "45%",
-                                padding: "0.25rem 1rem",
-                            }}
-                        >
-                            <CardContent>
-                                <Typography variant="h3">
-                                    Constraints & Assumptions:
-                                </Typography>
-                                <Typography>
-                                    {project.constraints_assumptions}
-                                </Typography>
-                            </CardContent>
-                        </Card>
-                    </Container>
-                    <Typography variant="body1" sx={{ marginTop: "1rem" }}>
-                        Project Team: {project.team_name}
-                    </Typography>
-                    <Typography>
-                        Created:{" "}
-                        {project.created_at
-                            ? new Date(project.created_at).toLocaleDateString(
-                                  "en-US",
-                                  {
-                                      year: "numeric",
-                                      month: "long",
-                                      day: "numeric",
-                                      hour: "numeric",
-                                      minute: "numeric",
-                                  }
-                              )
-                            : "Unknown Date"}
-                    </Typography>
-                    <Typography>
-                        Last updated:{" "}
-                        {project.updated_at
-                            ? new Date(project.updated_at).toLocaleDateString(
-                                  "en-US",
-                                  {
-                                      year: "numeric",
-                                      month: "long",
-                                      day: "numeric",
-                                      hour: "numeric",
-                                      minute: "numeric",
-                                  }
-                              )
-                            : "Unknown Date"}
-                    </Typography>
-                </Paper>
-            </Container>
-        </>
-    );
+      <Container maxWidth="lg" sx={{ py: 4 }}>
+        <Button
+          href="/projects"
+          startIcon={<ArrowBackOutlinedIcon />}
+          variant="outline-orange"
+        >
+          Back to Projects
+        </Button>
+        <Paper
+          sx={{
+            marginTop: "1rem",
+            padding: "1rem",
+          }}
+        >
+          <Container
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              px: "0",
+            }}
+          >
+            <Typography variant="h1">
+              {project.display_name || project.title}
+            </Typography>
+            <Button
+              startIcon={<EditOutlined />}
+              variant="solid-orange"
+              onClick={() => {
+                alert("Edit functionality not implemented yet");
+              }}
+            >
+              Edit Details
+            </Button>
+          </Container>
+          <Box
+            sx={{
+              marginBlock: "1rem",
+              padding: "0.5em 1em",
+              display: "inline-block",
+              backgroundColor:
+                project.status === "active"
+                  ? "rgba(0, 156, 189, 0.2)"
+                  : project.status === "in progress"
+                    ? "rgba(246, 190, 0, 0.2)"
+                    : project.status === "completed"
+                      ? "rgba(132, 189, 0, 0.2)"
+                      : "rgba(124, 135, 142, 0.2)",
+              color:
+                project.status === "active"
+                  ? theme.palette.info.main
+                  : project.status === "in progress"
+                    ? theme.palette.warning.main
+                    : project.status === "completed"
+                      ? theme.palette.success.main
+                      : "rgb(124, 135, 142)",
+            }}
+          >
+            <Typography sx={{ margin: "0" }}>
+              {project.status.toUpperCase()}
+            </Typography>
+          </Box>
+          <Typography>{project.description}</Typography>
+          <Container
+            disableGutters
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              px: "0",
+            }}
+          >
+            <Card
+              variant="outlined"
+              sx={{
+                width: "45%",
+                padding: "0.25rem 1rem",
+              }}
+            >
+              <CardContent>
+                <Typography variant="h3">Challenges:</Typography>
+                <Typography>{project.project_challenges}</Typography>
+              </CardContent>
+            </Card>
+            <Card
+              variant="outlined"
+              sx={{
+                width: "45%",
+                padding: "0.25rem 1rem",
+              }}
+            >
+              <CardContent>
+                <Typography variant="h3">Constraints & Assumptions:</Typography>
+                <Typography>{project.constraints_assumptions}</Typography>
+              </CardContent>
+            </Card>
+          </Container>
+          <Typography variant="body1" sx={{ marginTop: "1rem" }}>
+            Project Team: {project.team_name}
+          </Typography>
+          <Typography>
+            Created:{" "}
+            {project.created_at
+              ? new Date(project.created_at).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                })
+              : "Unknown Date"}
+          </Typography>
+          <Typography>
+            Last updated:{" "}
+            {project.updated_at
+              ? new Date(project.updated_at).toLocaleDateString("en-US", {
+                  year: "numeric",
+                  month: "long",
+                  day: "numeric",
+                  hour: "numeric",
+                  minute: "numeric",
+                })
+              : "Unknown Date"}
+          </Typography>
+        </Paper>
+      </Container>
+    </>
+  );
 }
--- apps/scoop-portal/ui/src/app/projects/page.js
+++ apps/scoop-portal/ui/src/app/projects/page.js
@@ -2,95 +2,97 @@
 
 import React, { useEffect, useState } from "react";
 import Header from "@components/Header";
-import { Box, Button, Card, Container, Typography } from "@mui/material";
-import baseTheme from "@styles/theme";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Typography,
+  useTheme,
+} from "@mui/material";
 
 export default function Projects() {
-    const [projects, setProjects] = useState([]);
+  const theme = useTheme();
+  const [projects, setProjects] = useState([]);
 
-    useEffect(() => {
-        console.log("API URL: ", process.env.NEXT_PUBLIC_API_URL);
-        const fetchProjects = async () => {
-            try {
-                const res = await fetch(
-                    `${process.env.NEXT_PUBLIC_API_URL}/api/project`
-                );
-                const data = await res.json();
-                setProjects(data);
-            } catch (err) {
-                console.error("Failed to fetch projects: ", err);
-            }
-        };
+  useEffect(() => {
+    console.log("API URL: ", process.env.NEXT_PUBLIC_API_URL);
+    const fetchProjects = async () => {
+      try {
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_API_URL}/api/project`
+        );
+        const data = await res.json();
+        setProjects(data);
+      } catch (err) {
+        console.error("Failed to fetch projects: ", err);
+      }
+    };
 
-        fetchProjects();
-    }, []);
+    fetchProjects();
+  }, []);
 
-    return (
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg" sx={{ py: 4 }}>
+        <Typography variant="h1" sx={{ mb: 4 }}>
+          Projects
+        </Typography>
         <>
-            <Header />
-            <Container>
-                <Typography variant="h1" sx={{ mb: 4 }}>
-                    Projects
+          {projects.map((project) => (
+            <Card
+              square
+              key={project.id}
+              sx={{
+                padding: "1em",
+                margin: "0.5rem",
+              }}
+            >
+              <Box
+                sx={{
+                  display: "flex",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Typography variant="h2">
+                  {project.display_name || project.title || "Unknown Project"}
                 </Typography>
-                <>
-                    {projects.map((project) => (
-                        <Card
-                            square
-                            key={project.id}
-                            sx={{
-                                padding: "1em",
-                                margin: "0.5rem",
-                            }}
-                        >
-                            <Box
-                                sx={{
-                                    display: "flex",
-                                    justifyContent: "space-between",
-                                }}
-                            >
-                                <Typography variant="h2">
-                                    {project.display_name ||
-                                        project.title ||
-                                        "Unknown Project"}
-                                </Typography>
-                                <Button
-                                    variant="solid-orange"
-                                    href={`/projects/${project.id}`}
-                                >
-                                    View
-                                </Button>
-                            </Box>
-                            <Box
-                                sx={{
-                                    padding: "0.25em 0.75em",
-                                    display: "inline-block",
-                                    backgroundColor:
-                                        project.status === "active"
-                                            ? "rgba(0, 156, 189, 0.2)"
-                                            : project.status === "in progress"
-                                              ? "rgba(246, 190, 0, 0.2)"
-                                              : project.status === "completed"
-                                                ? "rgba(132, 189, 0, 0.2)"
-                                                : "rgba(124, 135, 142, 0.2)",
-                                    color:
-                                        project.status === "active"
-                                            ? baseTheme.palette.info.main
-                                            : project.status === "in progress"
-                                              ? baseTheme.palette.warning.main
-                                              : project.status === "completed"
-                                                ? baseTheme.palette.success.main
-                                                : "rgb(124, 135, 142)",
-                                }}
-                            >
-                                <Typography sx={{ margin: "0" }}>
-                                    {project.status.toUpperCase()}
-                                </Typography>
-                            </Box>
-                            <Typography>{project.description}</Typography>
-                        </Card>
-                    ))}
-                </>
-            </Container>
+                <Button variant="solid-orange" href={`/projects/${project.id}`}>
+                  View
+                </Button>
+              </Box>
+              <Box
+                sx={{
+                  padding: "0.25em 0.75em",
+                  display: "inline-block",
+                  backgroundColor:
+                    project.status === "active"
+                      ? "rgba(0, 156, 189, 0.2)"
+                      : project.status === "in progress"
+                        ? "rgba(246, 190, 0, 0.2)"
+                        : project.status === "completed"
+                          ? "rgba(132, 189, 0, 0.2)"
+                          : "rgba(124, 135, 142, 0.2)",
+                  color:
+                    project.status === "active"
+                      ? theme.palette.info.main
+                      : project.status === "in progress"
+                        ? theme.palette.warning.main
+                        : project.status === "completed"
+                          ? theme.palette.success.main
+                          : "rgb(124, 135, 142)",
+                }}
+              >
+                <Typography sx={{ margin: "0" }}>
+                  {project.status.toUpperCase()}
+                </Typography>
+              </Box>
+              <Typography>{project.description}</Typography>
+            </Card>
+          ))}
         </>
-    );
+      </Container>
+    </>
+  );
 }
