98dcace84920814aa5eba65bbccbfdf23a227c8b
ast5382 <ast5382@rit.edu>
2025-07-28 14:50:33 -0400

    changed null state

--- apps/scoop-portal/ui/src/app/user-context/page.js
+++ apps/scoop-portal/ui/src/app/user-context/page.js
@@ -5,7 +5,7 @@ const UserContext = createContext();
 
 
 export const UserProvider = ({ children }) => {
-  const [user, setUser] = useState(null); 
+  const [user, setUser] = useState(); 
   
   return (
     <UserContext.Provider value={{ user, setUser }}>
