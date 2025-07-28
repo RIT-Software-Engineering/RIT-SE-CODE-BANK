52db571ab95c629f31568b43e6f93cfea8f652ba
SierraTran <sierra.tran@mail.com>
2025-07-28 13:02:09 -0400

    Implemented loading skeleton

--- apps/scoop-portal/ui/src/app/projects/loading.js
+++ apps/scoop-portal/ui/src/app/projects/loading.js
@@ -1,4 +1,53 @@
-import Skeleton from "../_components/Skeleton";
+"use client";
+import {
+  Box,
+  Button,
+  Card,
+  Container,
+  Skeleton,
+  Typography,
+  useTheme,
+} from "@mui/material";
+import Header from "@components/Header";
+
+function ProjectCardLoading() {
+  return (
+    <Card
+      square
+      sx={{
+        padding: "1em",
+        margin: "0.5rem",
+      }}
+    >
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: "space-between",
+          mb: "0.5rem",
+        }}
+      >
+        <Skeleton variant="rectangular">
+          <Typography variant="h2">Project Title</Typography>
+        </Skeleton>
+        <Skeleton variant="rectangular">
+          <Button variant="solid-orange">View</Button>
+        </Skeleton>
+      </Box>
+      <Skeleton
+        variant="rectangular"
+        sx={{
+          mb: "0.25rem",
+          display: "inline-block",
+        }}
+      >
+        <Typography sx={{ margin: "0" }}>Project Status</Typography>
+      </Skeleton>
+      <Typography>
+        <Skeleton variant="rectangular" sx={{ mb: "1rem" }} />
+      </Typography>
+    </Card>
+  );
+}
 
 /**
  * This is the loading component for the projects page.
@@ -7,14 +56,21 @@ import Skeleton from "../_components/Skeleton";
  * @returns {JSX.Element}
  */
 function ProjectsLoading() {
-    return (
-        <div>
-            <Skeleton className="w-[10ch] h-[2em]" />
-            <main>
-                <Skeleton className="w-[11ch] h-[1.5em]" />
-            </main>
-        </div>
-    );
+  const theme = useTheme();
+  return (
+    <>
+      <Header />
+      <Container maxWidth="lg" sx={{ py: 4 }}>
+        <Typography variant="h1" sx={{ mb: 4 }}>
+          <Skeleton variant="rectangular" height="2.5rem" width="9.5rem" />
+        </Typography>
+
+        <ProjectCardLoading />
+        <ProjectCardLoading />
+        <ProjectCardLoading />
+      </Container>
+    </>
+  );
 }
 
 export default ProjectsLoading;
