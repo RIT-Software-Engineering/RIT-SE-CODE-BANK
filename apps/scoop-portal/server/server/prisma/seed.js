f9a589c39ecdef24191d70e59b764afc647fb3c3
SierraTran <sierra.tran@mail.com>
2025-07-29 00:26:15 -0400

    Adjusted reference for users table and reordered data seeding

--- apps/scoop-portal/server/server/prisma/seed.js
+++ apps/scoop-portal/server/server/prisma/seed.js
@@ -7,39 +7,41 @@ const prisma = new PrismaClient();
 // Use prisma.<model> to interact with your database
 
 async function main() {
-    console.log("Clearing data");
-    await prisma.project.deleteMany();
-    await prisma.semester_Group.deleteMany();
-    await prisma.journal_Entry.deleteMany();
-    await prisma.user.deleteMany();
-    await prisma.fruit.deleteMany();
-    await prisma.application.deleteMany();
+  console.log("Clearing data");
+  await prisma.project.deleteMany();
+  await prisma.semester_Group.deleteMany();
+  await prisma.journal_Entry.deleteMany();
+  await prisma.users.deleteMany();
+  await prisma.fruit.deleteMany();
+  await prisma.application.deleteMany();
 
-    console.log("Seeding data");
-    //example data
-    await prisma.fruit.create({
-        data: { name: "Apple", color: "Red", size: "Medium" },
-    });
-    await prisma.semester_Group.createMany({
-        data: sampleSemesterGroups,
-    });
-    await prisma.project.createMany({
-        data: sampleProjects,
-    });
-    await prisma.journal_Entry.createMany({
-        data: sampleJournalEntries,
-    });
-    await prisma.user.createMany({
-        data: sampleUsers,
-    });
-    console.log("Drop and create finished.");
+  console.log("Seeding data");
+
+  //example data
+  await prisma.fruit.create({
+    data: { name: "Apple", color: "Red", size: "Medium" },
+  });
+  await prisma.semester_Group.createMany({
+    data: sampleSemesterGroups,
+  });
+  await prisma.project.createMany({
+    data: sampleProjects,
+  });
+  await prisma.users.createMany({
+    data: sampleUsers,
+  });
+  await prisma.journal_Entry.createMany({
+    data: sampleJournalEntries,
+  });
+
+  console.log("Drop and create finished.");
 }
 main()
-    .then(async () => {
-        await prisma.$disconnect();
-    })
-    .catch(async (e) => {
-        console.error(e);
-        await prisma.$disconnect();
-        process.exit(1);
-    });
+  .then(async () => {
+    await prisma.$disconnect();
+  })
+  .catch(async (e) => {
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
