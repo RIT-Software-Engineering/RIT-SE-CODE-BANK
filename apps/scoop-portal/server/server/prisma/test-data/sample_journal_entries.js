75eb33a07b3423b9bee6e03c4e1d85452553fcc5
SierraTran <sierra.tran@mail.com>
2025-07-29 00:26:54 -0400

    Commented out deprecated journal entry test data

--- apps/scoop-portal/server/server/prisma/test-data/sample_journal_entries.js
+++ apps/scoop-portal/server/server/prisma/test-data/sample_journal_entries.js
@@ -1,14 +1,14 @@
 export const sampleJournalEntries = [
-    {
-        id: 1,
-        date: new Date("2025-06-13T12:00:00Z"), // in UTC
-        contactee: "Alice Johnson",
-        notes: "Edit this note for testing.",
-    },
-    {
-        id: 2,
-        date: new Date("2025-06-20T10:30:00Z"), // in UTC
-        contactee: "John Smith",
-        notes: "",
-    },
+  // {
+  //     id: 1,
+  //     date: new Date("2025-06-13T12:00:00Z"), // in UTC
+  //     contactee: "Alice Johnson",
+  //     notes: "Edit this note for testing.",
+  // },
+  // {
+  //     id: 2,
+  //     date: new Date("2025-06-20T10:30:00Z"), // in UTC
+  //     contactee: "John Smith",
+  //     notes: "",
+  // },
 ];
