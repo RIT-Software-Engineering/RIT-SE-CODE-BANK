b9b9cf20cfa642c78ac5a4fd465181bba02196db
SierraTran <sierra.tran@mail.com>
2025-07-31 00:16:28 -0400

    Added fourth semester group

--- apps/scoop-portal/server/server/prisma/test-data/sample_semester_groups.js
+++ apps/scoop-portal/server/server/prisma/test-data/sample_semester_groups.js
@@ -1,23 +1,30 @@
 export const sampleSemesterGroups = [
-    {
-        id: 1,
-        name: "2018-19 Fall / Spring",
-        dept: "SE",
-        start_date: new Date("2018-08-27"),
-        end_date: new Date("2019-05-08"),
-    },
-    {
-        id: 2,
-        name: "2019-19 Spring / Summer",
-        dept: "SE",
-        start_date: new Date("2019-01-13"),
-        end_date: new Date("2019-08-07"),
-    },
-    {
-        id: 3,
-        name: "2019-20 Fall / Spring",
-        dept: "SE",
-        start_date: new Date("2024-05-27"),
-        end_date: new Date("2024-08-08"),
-    },
+  {
+    id: 1,
+    name: "2018-19 Fall / Spring",
+    dept: "SE",
+    start_date: new Date("2018-08-27"),
+    end_date: new Date("2019-05-08"),
+  },
+  {
+    id: 2,
+    name: "2019-19 Spring / Summer",
+    dept: "SE",
+    start_date: new Date("2019-01-13"),
+    end_date: new Date("2019-08-07"),
+  },
+  {
+    id: 3,
+    name: "2019-20 Fall / Spring",
+    dept: "SE",
+    start_date: new Date("2024-05-27"),
+    end_date: new Date("2024-08-08"),
+  },
+  {
+    id: 4,
+    name: "2024-24 Spring / Summer",
+    dept: "SE",
+    start_date: new Date("2024-01-13"),
+    end_date: new Date("2024-08-07"),
+  },
 ];
