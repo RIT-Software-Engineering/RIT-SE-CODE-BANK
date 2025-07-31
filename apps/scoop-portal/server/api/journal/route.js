0c55cff8d1d10888e2f4e74fda52f16624b39198
SierraTran <sierra.tran@mail.com>
2025-07-31 00:15:31 -0400

    Temporary routes for owner type filtering

--- apps/scoop-portal/server/api/journal/route.js
+++ apps/scoop-portal/server/api/journal/route.js
@@ -11,13 +11,16 @@ const prisma = new PrismaClient();
  * @param {Object} res - The response object to send all jurnal entries or an error
  */
 router.get("/", async (req, res) => {
-    try {
-        const entries = await prisma.journal_Entry.findMany();
-        res.status(200).json(entries);
-    } catch (error) {
-        console.error("Error fetching journal entries:", error);
-        res.status(500).json({ message: "Error fetching journal entries" });
-    }
+  try {
+    const entries = await prisma.journal_Entry.findMany();
+    res.status(200).json(entries);
+  } catch (error) {
+    console.error("Error fetching journal entries:", error);
+    res.status(500).json({
+      message: "Error fetching journal entries",
+      error: error.message,
+    });
+  }
 });
 
 /**
@@ -27,19 +30,19 @@ router.get("/", async (req, res) => {
  * @param {Object} res - The response object to send back the created entry or an error
  */
 router.post("/", async (req, res) => {
-    const { date, contactee, notes } = req.body;
-    try {
-        const newEntry = await prisma.journal_Entry.create({
-            data: { date, contactee, notes },
-        });
-        res.status(200).json(newEntry);
-    } catch (error) {
-        console.error("Error creating journal entry:", error);
-        res.status(500).json({
-            message: "Error creating journal entry",
-            error: error.message,
-        });
-    }
+  const { date, contactee, notes } = req.body;
+  try {
+    const newEntry = await prisma.journal_Entry.create({
+      data: { date, contactee, notes },
+    });
+    res.status(200).json(newEntry);
+  } catch (error) {
+    console.error("Error creating journal entry:", error);
+    res.status(500).json({
+      message: "Error creating journal entry",
+      error: error.message,
+    });
+  }
 });
 
 /**
@@ -49,21 +52,92 @@ router.post("/", async (req, res) => {
  * @param {Object} res - The response object to send back the updated journal entry or an error
  */
 router.put("/:id", async (req, res) => {
-    const { id } = req.params;
-    const { notes } = req.body;
-    try {
-        const updatedEntry = await prisma.journal_Entry.update({
-            where: { id: Number(id) },
-            data: { notes },
-        });
-        res.status(200).json(updatedEntry);
-    } catch (error) {
-        console.error("Error updating journal entry:", error);
-        res.status(500).json({
-            message: "Error updating journal entry",
-            error: error.message,
-        });
-    }
+  const { id } = req.params;
+  const { notes } = req.body;
+  try {
+    const updatedEntry = await prisma.journal_Entry.update({
+      where: { id: Number(id) },
+      data: { notes },
+    });
+    res.status(200).json(updatedEntry);
+  } catch (error) {
+    console.error("Error updating journal entry:", error);
+    res.status(500).json({
+      message: "Error updating journal entry",
+      error: error.message,
+    });
+  }
+});
+
+// HACK: Temporary Routes
+/**
+ * GET journal entries for scoopdinator
+ */
+router.get("/admin", async (req, res) => {
+  // Get search/filter parameters from url
+  const { searchParams } = req.nextUrl;
+  const semester_groupId = searchParams.get("semester_groupId");
+
+  // Develop whereClause for fitlering
+  const whereClause = { journal_owner_type: "admin" };
+  if (semester_group) {
+    whereClause.semester_groupId = semester_groupId;
+  }
+
+  // Develop orderByClause for ordering
+  const orderByClause = { date: "desc" };
+
+  // Fetch journal entries based on the two clauses
+  try {
+    const adminEntries = await prisma.journal_Entry.findMany({
+      where: whereClause,
+      orderBy: orderByClause,
+    });
+    res.status(200).json(adminEntries);
+  } catch (error) {
+    console.error("Error fetching the admin journal entries: ", error);
+    res.status(500).json({
+      message: "Error fetching admin journal entries",
+      error: error.message,
+    });
+  }
+});
+
+/**
+ * GET journal entries for scoopervisor
+ */
+router.get("/coach", async (req, res) => {
+  const whereClause = { journal_owner_type: "coach" };
+  try {
+    const coachEntries = await prisma.journal_Entry.findMany({
+      where: whereClause,
+    });
+    res.status(200).json(coachEntries);
+  } catch (error) {
+    console.error("Error fetching the coach journal entries: ", error);
+    res.status(500).json({
+      message: "Error fetching coach journal entries",
+      error: error.message,
+    });
+  }
+});
+
+/**
+ * GET journal entries for scooployee
+ */
+router.get("/student", async (req, res) => {
+  try {
+    const studentEntries = await prisma.journal_Entry.findMany({
+      where: { journal_owner_type: "student" },
+    });
+    res.status(200).json(studentEntries);
+  } catch (error) {
+    console.error("Error fetching the student journal entries: ", error);
+    res.status(500).json({
+      message: "Error fetching student journal entries",
+      error: error.message,
+    });
+  }
 });
 
 export default router;
