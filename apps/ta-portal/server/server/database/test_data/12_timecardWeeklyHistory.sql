6f7664d920cc8387ebdd3bc6b8c2b09072d77dd0
syth <cjdeni205@gmail.com>
2025-08-05 10:42:11 -0400

    TImecard fixes with new database

--- apps/ta-portal/server/server/database/test_data/12_timecardWeeklyHistory.sql
+++ apps/ta-portal/server/server/database/test_data/12_timecardWeeklyHistory.sql
@@ -1,8 +1,8 @@
 INSERT INTO `TimecardWeeklyHistory` (`id`, `jobPositionHistoryId`, `weekStartDate`, `isCurrentWeek`) VALUES
 -- For Charlie Coder (JobPositionHistoryId: 1)
-(1, 1, '2025-07-04', 0),  -- Past week
-(2, 1, '2025-07-11', 0),  -- Another past week
-(4, 1, '2025-07-18', 1),  -- The new CURRENT week
+(1, 1, '2025-07-04T00:00:00.000Z', FALSE),  -- Past week
+(2, 1, '2025-07-11T00:00:00.000Z', FALSE),  -- Another past week
+(4, 1, '2025-07-18T00:00:00.000Z', TRUE),  -- The new CURRENT week
 
 -- For Denise Designer (JobPositionHistoryId: 2)
-(3, 2, '2025-07-11', 1);
+(3, 2, '2025-07-11T00:00:00.000Z', TRUE);
