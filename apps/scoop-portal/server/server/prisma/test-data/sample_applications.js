a274c19442227657fdb0451a35d3a3ccb6eb9c18
SierraTran <sierra.tran@mail.com>
2025-08-07 13:20:11 -0400

    Initial commit for sample_applications.js
    * Contains one application

---
+++ apps/scoop-portal/server/server/prisma/test-data/sample_applications.js
@@ -0,0 +1,13 @@
+export const sampleApplications = [
+  {
+    lastName: "Miller",
+    firstName: "Adam",
+    ritEmail: "atm7256@rit.edu",
+    coopsCompleted: 0,
+    startSemester: "2018 Fall",
+    coursesTaken: "12",
+    pendingOffers: false,
+    rejectionLetters: true,
+    SEcoopInterest: true,
+  },
+];
