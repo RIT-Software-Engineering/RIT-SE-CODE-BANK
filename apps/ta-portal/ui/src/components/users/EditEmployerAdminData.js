de7a72e1188798092691155103dbe69db1573917
lc2410 <lc2410@g.rit.edu>
2025-08-07 23:56:53 -0400

    fixed bug with uid limits

--- apps/ta-portal/ui/src/components/users/EditEmployerAdminData.js
+++ apps/ta-portal/ui/src/components/users/EditEmployerAdminData.js
@@ -14,11 +14,16 @@ export default function EditEmployerAdminData({ register, errors }) {
     <fieldset className="space-y-4 animate-fade-in">
 
       {/* --- EDITABLE FIELDS --- */}
-        <InputField
+         <InputField
             id="uid"
             label="User ID"
+            type="text"
+            inputMode="numeric"
+            pattern="[0-9]*"
             placeholder="Enter User ID"
-            registerProps={register("uid", { required: "A User ID is required." })}
+            registerProps={register("uid", {required: "A User ID is required.",})}
+            maxLength={9}
+            required={true}
             error={errors.uid}
         />
         <InputField
@@ -26,6 +31,7 @@ export default function EditEmployerAdminData({ register, errors }) {
             label="First Name"
             placeholder="Enter First Name"
             registerProps={register("fname", { required: "First name is required." })}
+            required={true}
             error={errors.fname}
         />
         <InputField
@@ -33,6 +39,7 @@ export default function EditEmployerAdminData({ register, errors }) {
             label="Last Name"
             placeholder="Enter Last Name"
             registerProps={register("lname", { required: "Last name is required." })}
+            required={true}
             error={errors.lname}
         />
         <InputField
@@ -40,6 +47,7 @@ export default function EditEmployerAdminData({ register, errors }) {
             label="Email"
             placeholder="Enter Email"
             registerProps={register("email", { required: "An email address is required." })}
+            required={true}
             error={errors.email}
         />
         <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
@@ -48,6 +56,7 @@ export default function EditEmployerAdminData({ register, errors }) {
             label="Pronouns"
             placeholder="Enter Pronouns"
             registerProps={register("pronouns", { required: "Pronouns are required." })}
+            required={true}
             error={errors.pronouns}
             />
             <InputField
@@ -55,6 +64,7 @@ export default function EditEmployerAdminData({ register, errors }) {
             label="Department"
             placeholder="Enter Department"
             registerProps={register("department", { required: "Department is required." })}
+            required={true}
             error={errors.department}
             />
         </div>
