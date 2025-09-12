a8269f4f3b08019b5a6d3050a48083816b0e405f
Kevin Chen <kvn.c8888@gmail.com>
2025-09-12 15:56:47 -0400

    Added documentation

--- apps/ta-portal/ui/jest.config.js
+++ apps/ta-portal/ui/jest.config.js
@@ -1,11 +1,23 @@
 const nextJest = require('next/jest')
 
+/*
+* nextJest is a function that creates a jest config for a next.js project.
+* dir: './': This is the directory that contains the next.js project.
+* createJestConfig is a function that creates a jest config for a next.js project.
+*/
 const createJestConfig = nextJest({
   dir: './',
 })
 
+/*
+* customJestConfig is an object that contains the jest config for the next.js project.
+* testEnvironment: 'jsdom': This is the environment that jest will use to run the tests.
+*/
 const customJestConfig = {
   testEnvironment: 'jsdom',
 }
 
+/*
+* module.exports is used to export the jest config for the next.js project.
+*/
 module.exports = createJestConfig(customJestConfig)
