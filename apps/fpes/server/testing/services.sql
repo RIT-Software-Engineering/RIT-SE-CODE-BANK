1e8068a3849d77070aa4cb6553f40d5a8502f799
DeclanHarty <declanharty04@gmail.com>
2025-09-19 14:31:15 -0400

    Added a test insert

--- apps/fpes/server/testing/services.sql
+++ apps/fpes/server/testing/services.sql
@@ -1,11 +1,20 @@
 CREATE TABLE service (
     id INT PRIMARY_KEY AUTO_INCREMENT,
-    form_id INT FOREIGN_KEY,
-    service_type ENUM('internal', 'external')
-    title VARCHAR(40),
-    hours_worked INT,
-    other_contributions VARCHAR(250)
+    form_id INT FOREIGN_KEY NOT NULL,
+    service_type ENUM('internal', 'external') NOT NULL
+    title VARCHAR(40) NOT NULL,
+    hours_worked INT NOT NULL,
+    other_contributions VARCHAR(250) NOT NULL
 );
+
+INSERT INTO service (form_id, service_type, title, hours_worked, other_contributions)
+VALUES (
+    0,
+    'internal'
+    'Title',
+    50,
+    ''
+)
 
 
 
