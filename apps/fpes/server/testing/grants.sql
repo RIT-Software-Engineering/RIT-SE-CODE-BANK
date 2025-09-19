926d1a05df98d92d65a4089beeb579350dacd83d
conlan <cac9809@gmail.com>
2025-09-19 14:28:22 -0400

    grant fixes

--- apps/fpes/server/testing/grants.sql
+++ apps/fpes/server/testing/grants.sql
@@ -3,11 +3,11 @@ CREATE TABLE grants (
     category ENUM('Funded', 'In Submission', 'Declined', 'In Development') NOT NULL,
     title VARCHAR(255) NOT NULL,
     funder VARCHAR(255) NOT NULL,
-    amount DECIMAL(12,2) DEFAULT NULL,              
-    est_amount DECIMAL(12,2) DEFAULT NULL,          
+    amount INT(12,2) DEFAULT NULL,              
+    est_amount INT(12,2) DEFAULT NULL,          
     time_period VARCHAR(100) NOT NULL,              
     faculty_role VARCHAR(150) NOT NULL,             
-    faculty_share DECIMAL(12,2) DEFAULT NULL,       
+    faculty_share INT(12,2) DEFAULT NULL,       
     
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
