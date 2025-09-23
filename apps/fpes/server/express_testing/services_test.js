53c12ea6622c78cc61497c5318261e678bb93a06
DeclanHarty <declanharty04@gmail.com>
2025-09-23 11:06:06 -0400

    Added service route

--- apps/fpes/server/express_testing/services_test.js
+++ apps/fpes/server/express_testing/services_test.js
@@ -1,47 +1,26 @@
 const express = require('express');
-const app = express();
+const router = express.Router();
+const pool = require('../db')
 
 require('dotenv').config({path: "../.env"});
 
 const mariadb = require('mariadb')
 
-async function createMariaDBConnection(){
+router.get("/", async (req,res) => {
     let connection;
-    try{
-        connection = await mariadb.createConnection({
-        host: process.env.HOST,
-        user: process.env.DB_USERNAME,
-        password: process.env.DB_PASSWORD,
-        database: process.env.DATABASE
-        });
-        console.log("Made Connection...");
+    try {
+        connection = await pool.getConnection();
+        const results = connection.query("SELECT * FROM service");
+        res.json(results);
     } catch (err) {
-        
-        console.log("Error connecting to database...")
         console.log(err);
-    } finally{
-        if (connection){
-            return connection;
-        }
-
+        res.status(500).send("Error reading faculty information");
+    } finally {
+        if (connection) connection.release();
     }
-    
-}
+})
 
-async function main() {
-    let conn = await createMariaDBConnection();
-    let ping = await conn.query("SELECT * FROM service", (err, response) =>{
-        if (err) throw err;
-
-        console.log(response);
-    })
-    await console.log(ping);
-
-    conn.end();
-    console.log("Connection closed...")
-}
-
-main();
+module.exports = router;
 
 
 
--- apps/fpes/server/index.js
+++ apps/fpes/server/index.js
@@ -6,6 +6,9 @@ require('dotenv').config();
 const facultyRoutes = require('./express_testing/faculty_information_test');
 app.use('/faculty', facultyRoutes);
 
+const serviceRoutes = require('./express_testing/services_test');
+app.use('/service', serviceRoutes);
+
 // Test route (just to confirm server is alive)
 app.get('/', (req, res) => {
   res.send('Server is running...');
