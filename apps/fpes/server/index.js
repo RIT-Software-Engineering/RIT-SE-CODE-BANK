17f5681ed3690a70932d8306d393ac059b6d7e57
conlan <cac9809@gmail.com>
2025-09-26 13:07:10 -0400

    index.js student support

--- apps/fpes/server/index.js
+++ apps/fpes/server/index.js
@@ -16,7 +16,7 @@ app.use('/grants', grantsRoutes);
 const courseSectionRoutes = require('./routes/course_section_routes.js');
 app.use('/course_sections', courseSectionRoutes);
 
-const studentSupportRoutes = require('./routes/atudetn_support_routes.js');
+const studentSupportRoutes = require('./routes/student_support_routes.js');
 app.use('/student_support', studentSupportRoutes);
 
 // Test route (just to confirm server is alive)
