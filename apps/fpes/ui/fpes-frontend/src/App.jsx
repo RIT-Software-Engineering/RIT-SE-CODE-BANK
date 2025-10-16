21731defa9a1542fa2ca2cf50a23b9a6194896b0
DeclanHarty <declanharty04@gmail.com>
2025-10-16 11:00:24 -0400

    Added Example.env

--- apps/fpes/ui/fpes-frontend/src/App.jsx
+++ apps/fpes/ui/fpes-frontend/src/App.jsx
@@ -6,13 +6,14 @@ import './App.css'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import ServicesPage from './pages/services/ServicesPage.jsx';
 import GrantsTable from './pages/grants';
+import DepartmentsPage from './pages/departments/DepartmentsPage.jsx';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div>
-      <h1>HEADER</h1>
+      <h1>FPES Portal</h1>
       <BrowserRouter>
         <nav>
           <Link to="/services">Services</Link>
@@ -20,10 +21,14 @@ function App() {
         <nav>
           <Link to="/grants">Grants</Link>
         </nav>
+        <nav>
+          <Link to="/departments">Departments</Link>
+        </nav>
 
         <Routes>
           <Route path="/services" element={<ServicesPage/>} />
           <Route path="/grants" element={<GrantsTable />} />
+          <Route path="/departments" element={<DepartmentsPage/>}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ apps/fpes/ui/fpes-frontend/src/pages/departments/DepartmentsPage.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+
+export default function DepartmentsPage(){
+    const [departments, setDepartments] = useState([])
+
+    return(
+        <div>
+            <h3>Departments Page</h3>
+            
+        </div>
+    );
+}
