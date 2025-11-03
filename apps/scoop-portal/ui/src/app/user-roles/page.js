4bfabd697114254b936cd71f808ee4145a89be3a
Jose Diaz <jtd4400@web01apps02.rit.edu>
2025-11-03 11:22:06 -0500

    api

--- apps/scoop-portal/ui/src/app/user-roles/page.js
+++ apps/scoop-portal/ui/src/app/user-roles/page.js
@@ -12,7 +12,7 @@ export default function SelectUserPage() {
 
   useEffect(() => {
     const fetchUsers = async () => {
-      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/users`);
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/users`);
       const data = await res.json();
       setUsers(data);
     };
