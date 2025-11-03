0e29b02a8c2839753dfa9d893746edd02f35a78c
Jose Diaz <jtd4400@web01apps02.rit.edu>
2025-11-03 11:29:07 -0500

    route fix

--- apps/scoop-portal/server/server.js
+++ apps/scoop-portal/server/server.js
@@ -20,7 +20,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use("/api/semestergroup", semesterGroupRoutes);
 app.use("/api/application", applicationRoutes);
-app.use("/api/users", userRoutes);
+app.use("/api/scoop-portal/users", userRoutes);
 app.use("/api/journal", journalRoutes);
 app.use("/api/teams", teamRoutes);
 app.use("/api/auth", authRoutes);
