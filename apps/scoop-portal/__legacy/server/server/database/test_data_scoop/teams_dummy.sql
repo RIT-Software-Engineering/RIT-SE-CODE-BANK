98375054a04927f39e680773c160288f60353e03
Jack McCarthy <jackmc1112@gmail.com>
2025-11-04 13:41:54 -0500

    deleted scoop-portal for merge

--- apps/scoop-portal/__legacy/server/server/database/test_data_scoop/teams_dummy.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO teams (name, semester, project_id) VALUES
-('Team Alpha', 1, 1),
-('Team Beta', 1, 2);
--- apps/scoop-portal/ui/src/app/utils/LoginWrapper.js
+++
@@ -1,8 +0,0 @@
-import SelectUserPage from "user-roles/page";
-import ProdLogin from "user-login/page";
-
-export default function LoginWrapper() {
-  return process.env.NODE_ENV === 'development'
-    ? <SelectUserPage />
-    : <ProdLogin />;
-}
