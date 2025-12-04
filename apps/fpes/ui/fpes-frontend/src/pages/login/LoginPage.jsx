0e0b543459c3dfcce3acf337f0b17a82dc625819
Jay <jay1018lee@gmail.com>
2025-12-04 12:58:09 -0500

    FPES spelled out

--- apps/fpes/ui/fpes-frontend/src/pages/login/LoginPage.jsx
+++ apps/fpes/ui/fpes-frontend/src/pages/login/LoginPage.jsx
@@ -37,8 +37,8 @@ export default function LoginPage({ setRole, setIsAuthenticated, updateFacultyId
 
   return (
     <div style={{ textAlign: 'center', marginTop: '20px' }}>
-      <h1>FPES Portal</h1>
-      <Box sx={{ width: 300, margin: "auto", mt: 10 }}>
+      <h1>Faculty Performance <br /> Evaluation Portal</h1>
+      <Box sx={{ width: 250, margin: "auto", mt: 10 }}>
         <FormControl fullWidth
           sx={{ 
             '& .MuiInputLabel-shrink': {
