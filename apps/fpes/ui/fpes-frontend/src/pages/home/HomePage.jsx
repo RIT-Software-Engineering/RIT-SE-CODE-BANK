c707e2fcca9ec4ff39d9d31b19ac9caac31fa1aa
conlan <cac9809@gmail.com>
2025-12-05 13:20:34 -0500

    home page imporvements

--- apps/fpes/ui/fpes-frontend/src/pages/home/HomePage.jsx
+++ apps/fpes/ui/fpes-frontend/src/pages/home/HomePage.jsx
@@ -2,9 +2,12 @@ import React from "react";
 
 export default function HomePage() {
   return (
-    <div style={{ padding: "2rem" }}>
-      <h1>Welcome</h1>
-      <p>You are now logged in</p>
+    <div style={{ padding: "2rem"}}>
+      <h2>Welcome back</h2>
+      <p>You are now logged in to the Faculty Preformance Evaluation System</p>
+      <p>Navigate to one of the tabs above to start filling out a form</p>
     </div>
+
   );
+  
 }
