14fcafe96a7ef7bcb622273a2050b47e47011f27
DeclanHarty <declanharty04@gmail.com>
2025-12-06 17:49:56 -0500

    Added supervisor_id field and API calls for assigning supervisors and get a supervisors assigned faculty

--- apps/fpes/server/api/faculty_api.js
+++ apps/fpes/server/api/faculty_api.js
@@ -30,6 +30,41 @@ async function getFacultyById(facultyId) {
   }
 }
 
+// READ : get the faculty members a supervisor supervises
+async function getAllSupervisedFaculty(supervisor_id){
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const result = connection.query(
+      `SELECT * FROM faculty_information
+      WHERE supervisor_id = ?
+      `,
+      [supervisor_id]
+    )
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
+// READ : gets the record of a faculty members supervisor
+async function getSupervisorOfFacultyMember(facultyId){
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const result = connection.query(
+      `SELECT T2.* FROM T1 AS faculty_information 
+      INNER JOIN T2 as faculty_information
+      WHERE 
+      T1.faculty_id = ? AND
+      T1.supervisor_id = T2.faculty_id`,
+      [facultyId]
+    );
+    return result;
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
 // CREATE
 async function addFaculty({ name, rank, unit, affiliations = null, user_role }) {
   let conn;
@@ -74,6 +109,21 @@ async function updateFaculty(facultyId, data = {}) {
   }
 }
 
+// UPDATE : assign faculty member a supervisor
+async function assignSupervisorToFaculty(facultyId, supervisorId){
+  let connection;
+  try {
+    connection = await pool.getConnection();
+    const results = connection.query(
+      `UPDATE faculty_information SET supervisor_id = ? WHERE faculty_id = ?`,
+      [supervisorId, facultyId]
+    )
+    return results;
+  } finally {
+    if (connection) connection.release();
+  }
+}
+
 // DELETE
 async function deleteFaculty(facultyId) {
   let conn;
@@ -111,6 +161,11 @@ async function resetFacultyTable(){
         if (connection) connection.release();
     } 
 }
+
+
+
+
+
 
 module.exports = {
   getAllFaculty,
