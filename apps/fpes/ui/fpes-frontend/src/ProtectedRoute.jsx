c3125b6925613019e91bdfbc93f8c48f86b3c10d
Jay <jay1018lee@gmail.com>
2025-12-10 10:19:51 -0500

    re-added protected route

--- apps/fpes/ui/fpes-frontend/src/ProtectedRoute.jsx
+++ apps/fpes/ui/fpes-frontend/src/ProtectedRoute.jsx
@@ -3,8 +3,8 @@ import { Dialog, DialogTitle, DialogContent, DialogActions, Button } from "@mui/
 
 export default function ProtectedRoute({ isAuthenticated, children}) {
 
-  //if (!isAuthenticated) {
-  //  return <Navigate to="/login" replace />;
-  //}
+  if (!isAuthenticated) {
+   return <Navigate to="/login" replace />;
+  }
   return children;
 }
