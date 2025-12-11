51ee95be7146016bc82c6891d6fc3b584e330879
msk1582 <69879312+MSK1582@users.noreply.github.com>
2025-12-11 15:53:02 -0500

    matt broke dev and loves kc

--- scripts/deploy-scoop-portal.sh
+++ scripts/deploy-scoop-portal.sh
@@ -68,6 +68,12 @@ sleep 15
 npx prisma migrate deploy
 npx prisma db seed
 cd $APP_DIR
+cd $WORKFLOW_SERVER_DIR
+npx prisma db seed
+cd $APP_DIR
+cd $NOTIFICATIONS_SERVER_DIR
+npx prisma db seed
+cd $APP_DIR
 docker compose ps
 
 echo "SCOOP Portal deployment complete!"
