ca2151992a2899f0821b1ae8f4c22d137f28b305
Alana Wu <aw4857@rit.edu>
2026-02-13 11:18:50 -0500

    react and tailwind setup

---
+++ apps/byte-biters/src/App.jsx
@@ -0,0 +1,11 @@
+
+export default function App() {
+  return (
+    <div className="min-h-screen bg-black flex items-center justify-center">
+      <h1 className="text-5xl font-bold text-green-400">
+        Tailwind Is Working 🚀
+      </h1>
+    </div>
+  )
+}
+
