4218783a41a2e4eafe25deea410cf55efe24639b
francisjgrant <fjg5149@rit.edu>
2026-02-19 11:06:20 -0500

    back to original run

--- apps/cmt/cmt-project/deploy/run-cmt-dev.sh
+++ apps/cmt/cmt-project/deploy/run-cmt-dev.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-if [ -f docker-compose.dev.yml ]; then
-  docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
-else
-  docker compose -f docker-compose.yml up -d --build
-fi
+docker compose -f docker-compose.yml -f docker-compose.dev.yml up -d --build
+
