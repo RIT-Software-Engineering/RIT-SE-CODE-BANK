322599a2476a18a53327717e197fc433b1842144
francisjgrant <fjg5149@rit.edu>
2026-02-20 10:41:41 -0500

    updated env with new docker compose

--- apps/cmt/cmt-project/deploy/run-cmt-dev.sh
+++ apps/cmt/cmt-project/deploy/run-cmt-dev.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-docker compose -f docker-compose.yml up -d --build
+# docker compose -f docker-compose.yml up -d --build
+
+ENV_FILE=${1:-.env.development}
+
+docker compose --env-file $ENV_FILE -f docker-compose.yml up -d --build
