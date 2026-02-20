6913c43d3992ff7c57627863a70d6d066cd6cc43
francisjgrant <fjg5149@rit.edu>
2026-02-20 16:10:52 -0500

    env crap

--- apps/cmt/cmt-project/deploy/run-cmt-dev.sh
+++ apps/cmt/cmt-project/deploy/run-cmt-dev.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 # docker compose -f docker-compose.yml up -d --build
 
-ENV_FILE=${1:-.env.staging}
-
-docker compose --env-file $ENV_FILE -f docker-compose.yml up -d --build
+docker compose --env-file .env.staging -f docker-compose.yml up -d --build
