f46591f8d5ec90f3997facb9fa7ecd063a766db6
AydenDaz <aydend@optonline.net>
2026-02-20 10:54:55 -0500

    Added color wheel option when creating course

--- apps/cmt/cmt-project/src/pages/course/CourseOverview.jsx
+++ apps/cmt/cmt-project/src/pages/course/CourseOverview.jsx
@@ -1,8 +1,11 @@
 import { Check, Loader2, PlusIcon } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useEffect, useState, Fragment } from 'react'
 import { Button, Card, Col, Container, Form, Modal, Row } from 'react-bootstrap'
 import { CMTFetch } from '../../utils/api'
 import { useNavigate } from 'react-router-dom'
+import Wheel from '@uiw/react-color-wheel';
+import { hsvaToHex } from '@uiw/color-convert';
+
 
 export function CoursePageWorkflony() {
     const [courseOverview, setCourseOverview] = useState([{
@@ -54,6 +57,22 @@ export function CoursePageWorkflony() {
     )
 }
 
+
+function ColorWheel({setColor}) {
+    const [hsva, setHsva] = useState({ h: 122, s: 0, v: 90, a: 1 });
+    return (
+        <Fragment>
+        <div className='flex items-center'>
+            <Wheel color={hsva} onChange={(color) => {
+            setHsva(color.hsva)
+            setColor(hsvaToHex(color.hsva))
+            }} />
+            <div style={{ width: '50%', height: 34, marginTop: 20, background: hsvaToHex(hsva), marginLeft: "2rem"}}></div>
+        </div>
+        </Fragment>
+    );
+}
+
 function CourseCreationModal() {
     const [isOpen, setIsOpen] = useState(false)
 
@@ -63,10 +82,17 @@ function CourseCreationModal() {
 
     const [submitButtonElement, setSubmitButtonElement] = useState(<><PlusIcon />Create Course</>)
 
+    const [colorHidden, setColorHidden] = useState(true);
+    
+
     const navigate = useNavigate()
 
     function handleCourseCreation(e) {
         e.preventDefault()
+        if (color === "rainbow" || !color){
+            console.log("Pick a color!")
+            return false;
+        }
         setSubmitButtonElement(<><Loader2 className='animate-spin' />Creating...</>)
         CMTFetch('POST', '/course', { courseCode, courseName, color }).then(async response => {
             setSubmitButtonElement(<><Check />Created!</>)
@@ -79,7 +105,9 @@ function CourseCreationModal() {
         setCourseCode("")
         setCourseName("")
         setColor("")
+        setColorHidden(true)
     }
+
 
     return (
         <>
@@ -100,10 +128,12 @@ function CourseCreationModal() {
                         <Form.Label>Course Color</Form.Label>
                         <div className="flex gap-2 mb-4">
                             {/* // Colors are based of Open Colors, but adjusted using oklch.com to alter chroma/lightness to maintain contract for colorblind users */}
-                            {["#ff9749", "#ee605c", "#e64980", "#cb2d6a", "#405cc9", "#88e4bd", "#76d380"].map(hex =>
-                                <ColorRadioOption color={color} setColor={setColor} hex={hex}/>
+                            {["#ff9749", "#ee605c", "#e64980", "#cb2d6a", "#405cc9", "#88e4bd", "#76d380", "rainbow"].map(hex =>
+                                <ColorRadioOption color={color} setColor={setColor} hex={hex} setColorHidden={setColorHidden}/>
                             )}
+                            
                         </div>
+                        {colorHidden ? <></> : <div className='mb-3'>{<ColorWheel setColor={setColor}/>}</div>}
                         <Button type='submit'>
                             <div className='flex gap-1 -ml-1 mr-1'>
                                 {submitButtonElement}
@@ -122,24 +152,29 @@ function CourseCreationModal() {
     )
 }
 
-function ColorRadioOption({ color, setColor, hex}) {
+function ColorRadioOption({ color, setColor, hex, setColorHidden}) {
     return (
         <div key={hex}>
             <Form.Check
-                required
+                // required
                 type='radio'
                 name='color'
                 id={`color-${hex}`}
                 value={hex}
                 checked={color === hex}
-                onChange={e => setColor(e.target.value)}
+                onChange={e => { if (hex === "rainbow"){setColorHidden(false); setColor(e.target.value)} 
+                else{setColor(e.target.value); setColorHidden(true)}}}
                 className='d-none'
             />
             <label htmlFor={`color-${hex}`}>
                 <div
                     className="w-8 h-8 rounded-full cursor-pointer"
                     style={{
-                        backgroundColor: hex,
+                        backgroundImage:
+                            hex === "rainbow"
+                                ? "conic-gradient(red, orange, yellow, green, cyan, blue, violet, red)"
+                                : undefined,
+                        backgroundColor: hex !== "rainbow" ? hex : undefined,
                         border: color === hex ? `4px solid color-mix(in oklab, #eee, ${hex}` : '4px solid #eee',
                     }}
                 />
