463762a1d4a9814da331b6d863c6b236d1c068c0
francisjgrant <fjg5149@rit.edu>
2026-02-23 09:03:22 -0500

    old runt cmt

--- apps/cmt/cmt-project/deploy/run-cmt-dev.sh
+++ apps/cmt/cmt-project/deploy/run-cmt-dev.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-# docker compose -f docker-compose.yml up -d --build
+docker compose -f docker-compose.yml up -d --build
 
-docker compose --env-file .env.staging -f docker-compose.yml up -d --build
+# docker compose --env-file .env.staging -f docker-compose.yml up -d --build
