6170ebc9da25d0fc96dbf408d65688f052e25bb1
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2026-02-26 16:15:38 -0500

    fixed db scripts

--- scripts/deploy-ta-portal.sh
+++ scripts/deploy-ta-portal.sh
@@ -28,6 +28,7 @@ ssh -i "$DEPLOY_KEY" "kjk9042@apps-staging.se.rit.edu" << ENDSSH
     sleep 10
 
     echo "Migrating the database..."
+    docker compose exec backend npm run prisma:deploy
     
     echo "Checking service status..."
     docker compose ps
