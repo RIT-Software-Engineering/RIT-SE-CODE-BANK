d7c0818e2d7ce9266a72b4d9dcb0b4d03fecc532
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2026-02-27 16:28:48 -0500

    healthcheck and script comment out

--- apps/ta-portal/server/entrypoint.sh
+++ apps/ta-portal/server/entrypoint.sh
@@ -1,30 +1,30 @@
 #!/bin/sh
 set -e
 
-MAX_ATTEMPTS=30
-SLEEP_SECONDS=4
+# MAX_ATTEMPTS=30
+# SLEEP_SECONDS=4
 
-attempt=1
+# attempt=1
 
-echo "Wait for Database"
+# echo "Wait for Database"
 
-while [ "$attempt" -le "$MAX_ATTEMPTS" ];
-do
-    if printf "SELECT 1;" | npx prisma db execute --schema=/app/apps/ta-portal/server/server/database/prisma/schema.prisma --stdin >/dev/null 2>&1
-    then
-        echo "Database is ready"
-        break
-    fi
+# while [ "$attempt" -le "$MAX_ATTEMPTS" ];
+# do
+#     if printf "SELECT 1;" | npx prisma db execute --schema=/app/apps/ta-portal/server/server/database/prisma/schema.prisma --stdin >/dev/null 2>&1
+#     then
+#         echo "Database is ready"
+#         break
+#     fi
 
-    echo "Attempt $attempt/$MAX_ATTEMPTS failed. Retrying."
-    attempt=$((attempt + 1))
-    sleep "$SLEEP_SECONDS"
-done
+#     echo "Attempt $attempt/$MAX_ATTEMPTS failed. Retrying."
+#     attempt=$((attempt + 1))
+#     sleep "$SLEEP_SECONDS"
+# done
 
-if [ "$attempt" -gt "$MAX_ATTEMPTS" ]; then
-    echo "Database did not become ready in time."
-    exit 1
-fi
+# if [ "$attempt" -gt "$MAX_ATTEMPTS" ]; then
+#     echo "Database did not become ready in time."
+#     exit 1
+# fi
 
 echo "Running Prisma Migration"
 npm run prisma:deploy
