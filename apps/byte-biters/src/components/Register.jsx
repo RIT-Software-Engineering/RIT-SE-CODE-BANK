c1a9ad7b553aa35c53ae11582783f72941b0fd05
Alana <aw4857@rit.edu>
2026-03-02 14:29:38 -0500

    fixed file structure

--- apps/byte-biters/src/components/Register.jsx
+++
@@ -1,8 +0,0 @@
-export default function Register({name, number = "000"}) {
-    return (
-        <div>
-            <text className="pr-2 font-bold">{name}:</text>
-            <text className="bg-main-secondary border-border-secondary rounded-sm border-2 p-1 px-2">{number}</text>
-        </div>
-    )
-}
