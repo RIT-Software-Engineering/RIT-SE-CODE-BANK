06ec64a7fa9d8ad8051fcdb2c1bfc69868b9d6ae
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2026-03-03 12:58:48 -0500

    remove migration from deploy script

--- scripts/deploy-ta-portal.sh
+++ scripts/deploy-ta-portal.sh
@@ -26,9 +26,6 @@ ssh -i "$DEPLOY_KEY" "kjk9042@apps-staging.se.rit.edu" << ENDSSH
     
     echo "Waiting for services to be healthy..."
     sleep 10
-
-    echo "Migrating the database..."
-    docker compose exec backend npm run prisma:deploy
     
     echo "Checking service status..."
     docker compose ps
