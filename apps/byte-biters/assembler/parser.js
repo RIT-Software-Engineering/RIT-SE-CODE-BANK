0bb47a4c27baa4b2c2d83a96627292a9ea109519
SamCollier75 <sjc8315@rit.edu>
2026-03-05 17:10:29 -0500

    Parser is believed to be finished along with the creation of opcodes

--- apps/byte-biters/assembler/parser.js
+++ apps/byte-biters/assembler/parser.js
@@ -21,11 +21,89 @@ export function instructionLevel(tokenArray) {
     }
 }
 
-function parseOperand(tokens) {
+export function parseInstruction(tokens) {
+    const {mnemonic, srcTokens, dstTokens} = instructionLevel(tokens);
+
+    return {
+        mnemonic: mnemonic,
+        src: srcTokens ? parseOperand(srcTokens) : null,
+        dst: dstTokens ? parseOperand(dstTokens) : null
+    };
+}
+
+//Used to determine the type of mode given and to return the appropriate values for each
+export function parseOperand(tokens) {
+    //Used to return the mode and register value for a register mode
     if(tokens.length === 1 && isRegister(tokens[0])) {
         return {
             mode: "register",
             reg: getRegisterNumber(tokens[0])
+        };
+    }
+    //Used to return the mode and register value for a register deferred mode
+    else if(tokens.length === 2 && tokens[0] === "@" && isRegister(tokens[1])) {
+        return {
+            mode: "register_deferred",
+            reg: getRegisterNumber(tokens[1])
+        };
+    }
+    //Used to return the mode and register value for an autoincrement mode
+    else if(tokens.length === 4 && tokens[0] === "(" && isRegister(tokens[1]) && tokens[2] === ")" && tokens[3] === "+") {
+        return {
+            mode: "autoincrement",
+            reg: getRegisterNumber(tokens[1])
+        };
+    }
+    //Used to return the mode and register for an autoincrement deferred mode
+    else if(tokens.length === 5 && tokens[0] === "@" && tokens[1] === "(" && isRegister(tokens[2]) && tokens[3] === ")" && tokens[4] === "+") {
+        return {
+            mode: "autoincrement_deferred",
+            reg: getRegisterNumber(tokens[2])
+        };
+    }
+    //Used to return the mode and register for an autodecrement mode
+    else if(tokens.length === 4 && tokens[0] === "-" && tokens[1] === "(" && isRegister(tokens[2]) && tokens[3] === ")") {
+        return {
+            mode: "autodecrement",
+            reg: getRegisterNumber(tokens[2])
+        };
+    }
+    //Used to return the mode and register for an autodecrement deferred mode
+    else if(tokens.length === 5 && tokens[0] === "@" && tokens[1] === "-" && tokens[2] === "(" && isRegister(tokens[3]) && tokens[4] === ")") {
+        return {
+            mode: "autodecrement_deferred",
+            reg: getRegisterNumber(tokens[3])
+        };
+    }
+    //Both versions of index need to be edited to allow for the use of variables when those are implemented
+    //Used to return the mode, register, and offset from index for an indexed mode
+    else if(tokens.length === 4 && isNumber(tokens[0]) && tokens[1] === "(" &&  isRegister(tokens[2]) && tokens[3] === ")") {
+        return {
+            mode: "indexed",
+            reg: getRegisterNumber(tokens[2]),
+            offset: tokens[0]
+        };
+    }
+    //Used to return the mode, register, and offset from index for an indexed deferred mode
+    else if(tokens.length === 5 && tokens[0] === "@" && isNumber(tokens[1]) && tokens[2] === "(" &&  isRegister(tokens[3]) && tokens[4] === ")") {
+        return {
+            mode: "indexed_deferred",
+            reg: getRegisterNumber(tokens[3]),
+            offset: tokens[1]
+        };
+    }
+    //Used to return the mode and register value for an immediate mode
+    else if(tokens.length === 2 && tokens[0] === "#" && isNumber(tokens[1])) {
+        return {
+            mode: "immediate",
+            offset: tokens[1]
+        };
+    }
+    //Used to return the mode and register value for an absolute mode
+    else if(tokens.length === 3 && tokens[0] === "@" && tokens[1] === "#" && isNumber(tokens[2])) {
+        return {
+            mode: "absolute",
+            offset: tokens[2]
         };
     }
 }
@@ -37,6 +115,10 @@ function parseOperand(tokens) {
 
 function isRegister(token) {
     return /^R[0-7]$/.test(token);
+}
+
+function isNumber(token) {
+    return /^-?\d+$/.test(token);
 }
 
 function getRegisterNumber(token) {
--- apps/byte-biters/assembler/test.js
+++ apps/byte-biters/assembler/test.js
@@ -1,17 +1,19 @@
 import {index} from "./index.js";
 import {lexer} from "./lexer.js";
-import {instructionLevel} from "./parser.js";
+import {instructionLevel, parseInstruction} from "./parser.js";
 
 const indexOutput = index("   MOV R1     , R2      ; move R1 into R2\n" +
-                "ADD   #5, R3\n" +
+                "ADD   # 5, R3\n" +
                 "\n" +
                 "    ; a full-line comment\n" +
                 "SUB (R2)+, R4   ; subtract\n" +
                 "CLR;");
-const lexerOutput = lexer(indexOutput[1]);
-const parserOutput = instructionLevel(lexerOutput);
+const lexerOutput = lexer(indexOutput[2]);
+// const parserOutput = instructionLevel(lexerOutput);
+const instructionOutput = parseInstruction(lexerOutput);
 
 console.log(indexOutput);
 console.log(lexerOutput);
-console.log(parserOutput);
+// console.log(parserOutput);
+console.log(instructionOutput);
 
