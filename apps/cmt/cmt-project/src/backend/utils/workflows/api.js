43f393ec17b2da08aad85fb6f2a12316894f72c5
francisjgrant <fjg5149@rit.edu>
2026-03-10 13:33:06 -0400

    updated reverse for actions in api for node version 18

--- apps/cmt/cmt-project/src/backend/utils/workflows/api.js
+++ apps/cmt/cmt-project/src/backend/utils/workflows/api.js
@@ -228,12 +228,20 @@ async function objectToNewAction(action, ownerId, parentActionId) {
     parentActionId: parentActionId,
   })
 
+  // The below complex action won't work with node 18 which is what the staging enviorment uses
   // Link complex action's children
+  // if (action.childActions) {
+  //   for (const childAction of action.childActions.toReversed()) {
+  //     await objectToNewAction(childAction, ownerId, createdAction.id)
+  //   }
+  // }
+
+  // Updated version for the staging environment's node version
   if (action.childActions) {
-    for (const childAction of action.childActions.toReversed()) {
-      await objectToNewAction(childAction, ownerId, createdAction.id)
-    }
+  for (const childAction of [...action.childActions].reverse()) {
+    await objectToNewAction(childAction, ownerId, createdAction.id)
   }
+}
 
   return createdAction
 }
