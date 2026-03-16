ec8205de45e0d18a7c791a2541885f79b05cb915
Scapsters <scotty.happy@gmail.com>
2026-03-16 15:43:38 -0400

    add background

--- apps/cmt/cmt-project/src/pages/course/CourseDashboard.jsx
+++ apps/cmt/cmt-project/src/pages/course/CourseDashboard.jsx
@@ -138,7 +138,7 @@ function CourseInfo({ course, actionsWithContext, refresh, fetchToCallback }) {
             <div className='flex items-center mb-4'>
                 <Button onClick={() => navigate('/courses')}><div className='flex items-center'><ArrowLeft/>Back</div></Button>
             </div>
-            <div className="flex items-end h-16 gap-14 mt-2 w-max pb-2" style={{ borderBottomWidth: "6px", borderBottomColor: course.color }}>
+            <div className="flex items-end gap-14 mt-2 w-max py-2 pl-2 pr-8 rounded-t" style={{ borderBottomWidth: "6px", borderBottomColor: course.color, backgroundColor: `color-mix(in oklab, #fff 85%, ${course.color})` }}>
                 <div>
                     <p className="text-3xl mb-0">{course.classId}</p>
                     <div className="flex gap-10 text-gray-600">
