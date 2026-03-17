0678ec7bc7f6841369e40a8adc55ad49c5ba1e80
Alana <aw4857@rit.edu>
2026-03-17 16:34:54 -0400

    fixed register text error, fixed upper and lowercase handling

--- apps/byte-biters/frontend/src/components/Register.jsx
+++ apps/byte-biters/frontend/src/components/Register.jsx
@@ -1,8 +1,8 @@
 export default function Register({name, value = "0000"}) {
     return (
         <div>
-            <text className="pr-2 font-bold">{name}:</text>
-            <text className="bg-main-secondary border-border-secondary rounded-sm border-2 p-1 px-2">{value}</text>
+            <p className="pr-2 font-bold">{name}:</p>
+            <p className="bg-main-secondary border-border-secondary rounded-sm border-2 p-1 px-2">{value}</p>
         </div>
     )
 }
--- apps/byte-biters/frontend/src/components/RegisterPanel.jsx
+++ apps/byte-biters/frontend/src/components/RegisterPanel.jsx
@@ -3,7 +3,7 @@ import Register from "./Register";
 
 function RegisterPanel ({ registers }) {
     return (
-        <div className="bg-main-secondary h-24 flex flex-col items-center justify-between py-4 gap-4">
+        <div className="bg-main-secondary flex flex-col items-center justify-between py-4 gap-4">
             <div className="flex flex-row gap-8">
                 {registers.slice(0,6).map((reg, i) => (
                     <Register key={i} name={`R${i}`} value={reg.toString().padStart(4, "0")}/>
@@ -13,6 +13,7 @@ function RegisterPanel ({ registers }) {
             <div className="flex flex-row gap-8">
                 <Register key="sp" name="SP" value={registers[6].toString().padStart(4, "0")}/>
                 <Register key="pc" name="PC" value={registers[7].toString().padStart(4, "0")}/>
+                {/* fix this still! */}
                 <Register name="SR"></Register>
             </div>
             
