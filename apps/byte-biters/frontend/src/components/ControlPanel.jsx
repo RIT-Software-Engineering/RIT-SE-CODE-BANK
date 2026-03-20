13b18fa6dfcf7c0aa0c43401dfd306b7d4993f5e
Alana <aw4857@rit.edu>
2026-03-20 03:03:53 -0400

    fixed memory panel issue

--- apps/byte-biters/frontend/src/components/ControlPanel.jsx
+++ apps/byte-biters/frontend/src/components/ControlPanel.jsx
@@ -4,7 +4,7 @@ export default function ControlPanel({ onAssemble, onRun, onStepForward, onStepB
     return (
         //palceholder buttons, no functionality
         //add onclick later 
-        <div className="bg-s h-16 bg-main-secondary flex flex-row items-center justify-center gap-4 py-4">
+        <div className="bg-s h-full bg-main-secondary flex flex-row items-center justify-center gap-4 py-4">
             <Button variant="primary" onClick={onAssemble}>Assemble</Button>
             <Button variant="primary" onClick={onRun}> Run </Button>
             {/* remember to add backstep button here */}
