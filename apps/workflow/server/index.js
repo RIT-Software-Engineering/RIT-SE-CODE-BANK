db34597437ea1037bc531e8408ce08cdf42b13b8
zorinn <zorinmakabenta@live.com>
2026-03-21 23:33:38 -0400

    Fixed some changes so that environment changes are correct

--- apps/workflow/server/index.js
+++ apps/workflow/server/index.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: '.env' });
+const env = process.env.NODE_ENV || "development";
+require('dotenv').config({ path: `.env.${env}` });
 
 const express = require('express');
 const app = express();
--- services/notification-service/src/index.js
+++ services/notification-service/src/index.js
@@ -5,7 +5,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 
 const env = process.env.NODE_ENV || "development";
-dotenv.config({ path: `../.env.${env}` });
+dotenv.config({ path: `.env.${env}` });
 
 const app = express();
 
