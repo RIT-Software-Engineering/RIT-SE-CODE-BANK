3ea57bce69f929e0df689e56125eaa536227ce3a
Alana <aw4857@rit.edu>
2026-03-24 18:47:59 -0400

    Backstep button connected and status register and flags are set up, still needs to be properly tested

--- apps/byte-biters/frontend/src/components/Register.jsx
+++ apps/byte-biters/frontend/src/components/Register.jsx
@@ -1,6 +1,6 @@
 export default function Register({name, value = "0000"}) {
     return (
-        <div>
+        <div className="flex flex-row">
             <p className="pr-2 font-bold">{name}:</p>
             <p className="bg-main-secondary border-border-secondary rounded-sm border-2 p-1 px-2">{value}</p>
         </div>
--- apps/byte-biters/frontend/src/components/RegisterPanel.jsx
+++ apps/byte-biters/frontend/src/components/RegisterPanel.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 import Register from "./Register";
 
-function RegisterPanel ({ registers }) {
+function RegisterPanel ({ registers, flags }) {
     return (
         <div className="bg-main-secondary flex flex-col items-center justify-between py-4 gap-4">
             <div className="flex flex-row gap-8">
@@ -13,10 +13,22 @@ function RegisterPanel ({ registers }) {
             <div className="flex flex-row gap-8">
                 <Register key="sp" name="SP" value={registers[6].toString().padStart(4, "0")}/>
                 <Register key="pc" name="PC" value={registers[7].toString().padStart(4, "0")}/>
-                {/* fix this still! */}
-                <Register name="SR"></Register>
+                <div className="flex flex-row items-center">
+                    <p className="pr-2 font-bold">SR:</p>
+                    <div className="flex gap-2 flex-row bg-main-secondary border-border-secondary rounded-sm border-2 p-1 px-3 font-mono text-sm">
+                        {["N ", "Z ", "V ", "C "].map((flag) => (
+                        <div key={flag} className="flex flex-row items-center">
+                            <span className="text-xs text-gray-400">{flag} </span>
+                                <span className={
+                                    flags?.[flag]
+                                    ? "text-button-default font-bold"
+                                    : "text-gray-500" }>
+                                {flags?.[flag] ? " 1" : " 0"}
+                            </span>
+                        </div> ))}
+                    </div>
+                </div>
             </div>
-            
         </div>
     )
 }
