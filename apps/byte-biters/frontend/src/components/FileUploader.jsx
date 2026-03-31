5d5eedfc9b1fe61c5288af2d27e1573a8b05c9b7
Alana <aw4857@rit.edu>
2026-03-31 15:35:38 -0400

    code fully documented

--- apps/byte-biters/frontend/src/components/FileUploader.jsx
+++ apps/byte-biters/frontend/src/components/FileUploader.jsx
@@ -1,18 +1,22 @@
 import React, { useRef, useState } from "react";
 import Button from "./Button";
 
+//component that handles when a user uploads a file to the code editor
 export default function FileUploader ({setCode, onUploadComplete}){
     const fileInputRef = useRef(null);
     const [file, setFile] = useState(null);
 
+    //when the Open File button is clicked, it prompts the user to choose a file
     const handleButtonClick = () => {
         fileInputRef.current.click();
     };
 
+    //grabs the file and changes the code editor to its contents
     function handleFileChange(e) {
         const file = e.target.files[0];
 
         if (file){
+            //safety net in case the file is not a text file
             if (!file.name.endsWith(".txt")){
                 alert("Please upload a .txt file");
                 return;
@@ -24,7 +28,9 @@ export default function FileUploader ({setCode, onUploadComplete}){
 
         reader.onload = (event) => {
             const text = event.target.result;
+            //this is where the text is sent to the code editor
             setCode(text);
+            //for closing the dropdown
             if (onUploadComplete) {
                 onUploadComplete();
             }
@@ -35,10 +41,12 @@ export default function FileUploader ({setCode, onUploadComplete}){
     
     return (
         <div>
+            {/* dropdown button named open file */}
             <Button onClick={handleButtonClick} className="absolute top-full bg-main-secondary py-3 px-2 border-b-2 border-border-primary rounded shadow-lg z-10 w-40">
-                Upload File</Button>
+                Open File</Button>
 
-            <input type="file" accept=".txt,.asm" ref={fileInputRef}  onChange={handleFileChange} className="hidden"/>
+            {/* actual functionality */}
+            <input type="file" accept=".txt" ref={fileInputRef}  onChange={handleFileChange} className="hidden"/>
         </div>
     )
 }
--- apps/byte-biters/frontend/src/components/Header.jsx
+++ apps/byte-biters/frontend/src/components/Header.jsx
@@ -2,7 +2,7 @@ import Button from "./Button";
 import FileUploader from "./FileUploader";
 import { useState } from "react"
 
-//header, includes the add file and external resources buttons/links
+//header, includes the file and external resources buttons/links
 export default function Header({setCode}) {
     const [isOpen, setIsOpen] = useState(false)
 
@@ -10,11 +10,14 @@ export default function Header({setCode}) {
         <div>
         <div className="p-6 flex bg-main-primary min-h-20 justify-between">
             <div className="relative ">
-                <Button className="font-mono text-text-muted text-lg text-center px-2" onClick={() => setIsOpen(!isOpen)}>Add File! +</Button>
+                {/* file button on the top left of the page, opens a dropdown */}
+                <Button className="font-mono text-text-muted text-lg text-center px-2" onClick={() => setIsOpen(!isOpen)}>File</Button>
                 {isOpen && (
+                    // refer to FileUploader component
                     <FileUploader setCode={setCode} onUploadComplete={() => setIsOpen(false)} ></FileUploader>
                 )}
             </div>
+            {/* external resources button on the top right of the page, has no current functionality */}
             <Button className="font-mono text-text-muted text-lg"> External Resources</Button>
         </div>
         
