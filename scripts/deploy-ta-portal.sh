175a2ca52f88a30f159e8bb32c3eeb1f51731992
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2026-04-07 10:12:44 -0400

    fixed deploy script

--- scripts/deploy-ta-portal.sh
+++ scripts/deploy-ta-portal.sh
@@ -27,12 +27,6 @@ ssh -i "$DEPLOY_KEY" "kjk9042@apps-staging.se.rit.edu" \
 
     docker compose --env-file ".env.staging" -f compose.build.yaml -f compose.run.yaml up -d --build
     
-    echo "Waiting for services to be healthy..."
-    sleep 10
-    
-    echo "Checking service status..."
-    docker compose ps
-    
     echo "Deployment complete!"
 ENDSSH
 
