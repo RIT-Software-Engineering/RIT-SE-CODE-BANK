a45f7c995cd94c43e1acae0983ec7593e99ba242
KinkadeKS <kinkadejknoxsilvia@gmail.com>
2026-04-09 15:31:36 -0400

    production changes

--- scripts/deploy-notification-service.sh
+++ scripts/deploy-notification-service.sh
@@ -27,7 +27,7 @@ ssh -i "$DEPLOY_KEY" "kjk9042@apps-staging.se.rit.edu" \
 
     echo "Rebuilding and restarting Docker containers..."
     cd ./services/notification-service
-    docker compose -f compose.yaml -f compose.staging.yaml up -d --build
+    docker compose -f compose.build.yaml -f compose.run.yaml -f compose.dev.yaml up -d --build
     
     echo "Waiting for services to be healthy..."
     sleep 10
