8d1123e4269e869bc83c65dc8d8e0f7470c5852d
Scapsters <scotty.happy@gmail.com>
2026-04-13 15:04:37 -0400

    add staging and change backend dockerfile

--- apps/workflow/server/index.js
+++ apps/workflow/server/index.js
@@ -11,9 +11,13 @@ const permissionRoutes = require('./api/routes/permissions');
 
 const port = process.env.PORT || 5001;
 
+const allowedOrigins = []
+if (process.env.CMT_BASE_URL) allowedOrigins.push(process.env.CMT_BASE_URL)
+if (process.env.CMT_BASE_URL_STAGING) allowedOrigins.push(process.env.CMT_BASE_URL_STAGING)
+
 app.use(
   cors({
-    origin: process.env.BASE_URL || "http://localhost:3000",
+    origin: allowedOrigins || "http://localhost:3000",
     credentials: true,
   }),
 );
