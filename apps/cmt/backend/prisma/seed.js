556d146163fd05003363bdfeebcaacb9f9b4322d
Scapsters <scotty.happy@gmail.com>
2026-04-14 11:12:07 -0400

    fix import again

--- apps/cmt/backend/prisma/seed.js
+++ apps/cmt/backend/prisma/seed.js
@@ -2,7 +2,7 @@
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from './generated/client/index.js';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
