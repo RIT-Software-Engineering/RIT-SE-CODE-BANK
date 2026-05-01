1f8590c70bda85a63d8ca4b24cb2929740d85fdd
francisjgrant <fjg5149@rit.edu>
2026-05-01 15:41:28 -0400

    try again

--- apps/cmt/frontend/src/utils/LoginWrapper.js
+++ apps/cmt/frontend/src/utils/LoginWrapper.js
@@ -1,7 +1,7 @@
 "use client";
 
-import DevLoginPage from "../pages/DevLoginPage";
-import ShibbLoginPage from '../pages/ShibbLoginPage';
+import DevLoginPage from "../pages/DevLoginPage.jsx";
+import ShibbLoginPage from '../pages/ShibbLoginPage.jsx';
 
 /**
  * A wrapper component that conditionally renders the correct login system
