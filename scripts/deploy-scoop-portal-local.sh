401660545f6921b355fdc882b6c221798dab961e
zorinn <zim1902@rit.edu>
2026-05-05 14:10:13 -0400

    Changes to local scoop portal deploy script

--- scripts/deploy-scoop-portal-local.sh
+++ scripts/deploy-scoop-portal-local.sh
@@ -1,6 +1,6 @@
 set -e
 
-APP_DIR="/d/RIT-SE-CODE-BANK"
+APP_DIR="/opt/scoop-portal"
 BRANCH="scoop-portal-dev"
 PORTAL_SERVER_DIR="$APP_DIR/apps/scoop-portal/server"
 PORTAL_UI_DIR="$APP_DIR/apps/scoop-portal/ui"
@@ -87,7 +87,7 @@ npx prisma db seed
 cd $APP_DIR
 cd $WORKFLOW_SERVER_DIR
 npx prisma migrate deploy
-npx prisma db seed
+#npx prisma db seed
 cd $APP_DIR
 cd $NOTIFICATIONS_SERVER_DIR
 npx prisma migrate deploy
